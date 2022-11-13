a93b87b1374ac6e19901dd31335ea88bcd36d5f4
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-11-13 17:51:39 +0100

    changed naming, less nesting, added comments made some loops easier

--- cogs/keywords/KeywordSend.py
+++ cogs/keywords/KeywordSend.py
@@ -1,11 +1,9 @@
 ###package#import###############################################################################
 
 import nextcord
-from nextcord import Interaction
-from nextcord.ext import commands
 import re
 
-client = commands.Bot(intents=nextcord.Intents.all())
+client = nextcord.ext.commands.Bot(intents=nextcord.Intents.all())
 
 ###self#imports###############################################################################
 
@@ -17,63 +15,70 @@ from utilities.variables import BOT_COLOR
 
 
 
-class keyword(commands.Cog):
+class KeywordSend(nextcord.ext.commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
     ###keyword#noti###########################################################
 
-    @commands.Cog.listener()
+    @nextcord.ext.commands.Cog.listener()
     async def on_message(self,
-                         interaction: Interaction):
-        if not checks(interaction):
+                         message):
+        if not checks(message.guild, message.author):
             return
 
-        if not checks_forbidden_channels(interaction.channel):
+        if not checks_forbidden_channels(message.channel):
             return
 
-        all_users_keywords = get_keywords(interaction.author.id)
+        all_users_keywords = get_keywords(message.author.id)
 
-        for key in all_users_keywords.keys():
-            if not all_users_keywords[key] == []:
-                all_user_keywords = all_users_keywords[key]
-                all_user_keywords_in_content = []
+        #TODO only add keywords of users in this server
+        for user_id in all_users_keywords.keys():
+            #check if user is in server
+            if message.guild.get_member(user_id) != None:
+                #check if user has keywords
+                if not all_users_keywords[user_id] == []:
+                    all_user_keywords = all_users_keywords[user_id]
+                    all_user_keywords_in_content = []
 
-                for i in range(len(all_user_keywords)):
-                    if all_user_keywords[i] in re.sub(":.*?:", "", str(interaction.content.lower())):
-                        all_user_keywords_in_content.append(all_user_keywords[i])
+                    for keyword in all_user_keywords:
+                        if keyword in re.sub(":.*?:", "", str(message.content.lower())): # regex -> message without emotes
+                            all_user_keywords_in_content.append(keyword)
 
-                if len(all_user_keywords_in_content) > 0:
-                    output_keywords = ""
-                    last_keyword = all_user_keywords_in_content.pop()
+                    if all_user_keywords_in_content != []:
+                        output_keywords = ""
+                        last_keyword = all_user_keywords_in_content.pop()
 
-                    if not all_user_keywords_in_content == []:
-                        nearly_all_keywords_list = ", ".join(map(str,all_user_keywords_in_content))
-                        output_keywords = f"{nearly_all_keywords_list} and {last_keyword}"
-                        multiple_keywords = True
-                    else:
-                        output_keywords = last_keyword
-                        multiple_keywords = False
+                        #check if it's only 1 keyword
+                        if not all_user_keywords_in_content == []:
+                            nearly_all_keywords_list = ", ".join(map(str,all_user_keywords_in_content))
+                            output_keywords = f"{nearly_all_keywords_list} and {last_keyword}"
+                            multiple_keywords = True
+                        else:
+                            output_keywords = last_keyword
+                            multiple_keywords = False
 
-                    noti_user = await self.client.fetch_user(key)
+                        noti_user = await self.client.fetch_user(user_id)
 
-                    print(f"{interaction.author}: keyword() {noti_user}\n{output_keywords}")
+                        print(f"{message.author}: keyword() {noti_user}\n{output_keywords}")
 
-                    if multiple_keywords:
-                        keywords_info = f"Your keywords: `{output_keywords}` have been mentioned in {interaction.channel.mention} by {interaction.author.mention}:"
-                    else:
-                        keywords_info = f"Your keyword: `{output_keywords}` has been mentioned in {interaction.channel.mention} by {interaction.author.mention}:"
+                        if multiple_keywords:
+                            keywords_info = f"Your keywords: `{output_keywords}` have been mentioned in {message.channel.mention} by {message.author.mention}:"
+                        else:
+                            keywords_info = f"Your keyword: `{output_keywords}` has been mentioned in {message.channel.mention} by {message.author.mention}:"
 
-                    embed = embed_builder(title = f"Keyword Notification: `{output_keywords}`",
-                                          title_url = interaction.jump_url,
-                                          description = f"{keywords_info}\n\n__**Message:**__\n{interaction.content}"[:4096],
-                                          color = BOT_COLOR,
-                                          footer = "DEFAULT_KST_FOOTER",)
+                        embed = embed_builder(title = f"Keyword Notification: `{output_keywords}`",
+                                              title_url = message.jump_url,
+                                              description = f"{keywords_info}\n\n__**Message:**__\n{message.content}"[:4096],
+                                              color = BOT_COLOR,
+                                              footer = "DEFAULT_KST_FOOTER")
 
-                    await embed_attachments(noti_user, interaction, embed, link_embed = False)
+                        await embed_attachments(noti_user, message, embed)
 
-                    uses_update("command_uses", "keyword send")
+                        uses_update("command_uses", "keyword send")
+
+
 
 def setup(client):
-    client.add_cog(keyword(client))
+    client.add_cog(KeywordSend(client))

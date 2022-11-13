643599bc8ed7026b8dc9661ddf099af1cbea067b
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-11-13 18:06:25 +0100

    removed /sf and added localization for it made spotipy data import local used list comprehension to make artists_with_urls smaller

--- cogs/music/Spotify.py
+++ cogs/music/Spotify.py
@@ -1,21 +1,19 @@
 ###package#import###############################################################################
 
-import os
+import dotenv
 import nextcord
-from nextcord import Interaction, SlashOption, Spotify
-from nextcord.ext import commands
+import os
 import spotipy
-from dotenv import load_dotenv
 
-load_dotenv()
+dotenv.load_dotenv()
 
-client = commands.Bot(intents=nextcord.Intents.all())
+client = nextcord.ext.commands.Bot(intents=nextcord.Intents.all())
 
 ###self#imports###############################################################################
 
 from database.database_command_uses import uses_update
 from utilities.maincommands import checks
-from utilities.partial_commands import get_nick_else_name, get_spotify_track_data, embed_builder
+from utilities.partial_commands import get_nick_else_name, embed_builder
 from utilities.variables import SPOTIFY_ICON
 
 
@@ -30,19 +28,19 @@ spotifyOAuth = spotipy.SpotifyOAuth(client_id=os.environ["SPOTIPY_CLIENT_ID"],
 token = spotifyOAuth.get_cached_token()
 spotifyObject = spotipy.Spotify(auth=token['access_token'])
 
-class spotify(commands.Cog):
+class Spotify(nextcord.ext.commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
     ###spotify###########################################################
 
-    @nextcord.slash_command(name='spotify', description='tells you what someone is listening to on Spotify')
+    @nextcord.slash_command(name='sf', description='tells you what someone is listening to on Spotify', name_localizations = {country_tag:"spotify" for country_tag in nextcord.Locale})
     async def spotify(self,
-                      interaction: Interaction,
+                      interaction: nextcord.Interaction,
                       *,
-                      member: nextcord.Member = SlashOption(description="the user listening to Spotify", required=False)):
-        if not checks(interaction):
+                      member: nextcord.Member = nextcord.SlashOption(description="the user listening to Spotify", required=False)):
+        if not checks(interaction.guild, interaction.user):
             return
 
         if member == None:
@@ -56,88 +54,41 @@ class spotify(commands.Cog):
         spotifyObject = spotipy.Spotify(auth=token['access_token'])
 
         for activity in member.activities:
-            if isinstance(activity, Spotify):
-                track_spotipy = spotifyObject.track(f"spotify:track:{activity.track_id}")
+            if not isinstance(activity, nextcord.Spotify):
+                continue
 
-                track_url, track_name, album_url, album_name, artist_urls, artists_list, cover_url = get_spotify_track_data(track_spotipy)
+            track_spotipy = spotifyObject.track(f"spotify:track:{activity.track_id}")
 
-                artists_with_urls = []
-                i = 0
+            artists_with_urls = [f"[{artist['name']}]({artist['external_urls']['spotify']})" for artist in track_spotipy['artists']]
 
-                while i < len(artists_list):
-                    artists_with_urls.append(f"[{artists_list[i]}]({artist_urls[i]})")
-                    i += 1
+            album_url = track_spotipy['album']['external_urls']['spotify']
+            album_name = track_spotipy['album']['name']
 
-                artists = ", ".join(map(str,artists_with_urls))
+            track_url = track_spotipy['external_urls']['spotify']
+            track_name = track_spotipy['name']
 
-                embed = embed_builder(description = f"[{track_name}]({track_url})\non [{album_name}]({album_url})\nby {artists}",
-                                      color = activity.color,
-                                      thumbnail = cover_url,
-                                      author = f"{name} is listening to:",
-                                      author_icon = SPOTIFY_ICON,
-                                      footer = "DEFAULT_KST_FOOTER")
+            cover_url = track_spotipy['album']['images'][0]['url']
 
-                await interaction.send(embed=embed)
+            artists = ", ".join(map(str, artists_with_urls))
 
-                uses_update("command_uses", "spotify")
+            embed = embed_builder(description = f"[{track_name}]({track_url})\non [{album_name}]({album_url})\nby {artists}",
+                                  color = activity.color,
+                                  thumbnail = cover_url,
+                                  author = f"{name} is listening to:",
+                                  author_icon = SPOTIFY_ICON,
+                                  footer = "DEFAULT_KST_FOOTER")
 
-                return
+            await interaction.response.send_message(embed=embed)
+
+            uses_update("command_uses", "spotify")
+
+            return
 
         await interaction.response.send_message(f"`{name}` isn't listening to anything on Spotify right now.", ephemeral=True)
 
         uses_update("command_uses", "spotify")
 
-    ###spotify#alias###########################################################
 
-    @nextcord.slash_command(name='sf', description='tells you what someone is listening to on Spotify')
-    async def sf(self,
-                 interaction: Interaction,
-                 *,
-                 member: nextcord.Member = SlashOption(description="the user listening to Spotify (alias of /spotify)", required=False)):
-        if not checks(interaction):
-            return
-
-        if member == None:
-            member = interaction.guild.get_member(interaction.user.id)
-        
-        name = get_nick_else_name(member)
-
-        print(f"{interaction.user}: /sf {member}")
-
-        token = spotifyOAuth.get_cached_token()
-        spotifyObject = spotipy.Spotify(auth=token['access_token'])
-
-        for activity in member.activities:
-            if isinstance(activity, Spotify):
-                track_spotipy = spotifyObject.track(f"spotify:track:{activity.track_id}")
-
-                track_url, track_name, album_url, album_name, artist_urls, artists_list, cover_url = get_spotify_track_data(track_spotipy)
-
-                artists_with_urls = []
-                i = 0
-
-                while i < len(artists_list):
-                    artists_with_urls.append(f"[{artists_list[i]}]({artist_urls[i]})")
-                    i += 1
-
-                artists = ", ".join(map(str,artists_with_urls))
-                
-                embed = embed_builder(description = f"[{track_name}]({track_url})\non [{album_name}]({album_url})\nby {artists}",
-                                      color = activity.color,
-                                      thumbnail = cover_url,
-                                      author = f"{name} is listening to:",
-                                      author_icon = SPOTIFY_ICON,
-                                      footer = "DEFAULT_KST_FOOTER")
-
-                await interaction.send(embed=embed)
-
-                uses_update("command_uses", "sf")
-
-                return
-
-        await interaction.response.send_message(f"`{name}` isn't listening to anything on Spotify right now.", ephemeral=True)
-
-        uses_update("command_uses", "sf")
 
 def setup(client):
-    client.add_cog(spotify(client))
+    client.add_cog(Spotify(client))

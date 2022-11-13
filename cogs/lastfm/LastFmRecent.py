17823a5e169cae4f78bd4e9eb0255a0151856474
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-11-13 17:59:05 +0100

    exported buttons added page button and kst footer improved loops by removing try excepts moved lastfm username import from db to before recursion changed status 200 error on api call

--- cogs/lastfm/LastFmRecent.py
+++ cogs/lastfm/LastFmRecent.py
@@ -1,88 +1,31 @@
 ###package#import###############################################################################
 
 import nextcord
-from nextcord import Interaction, SlashOption
-from nextcord.ext import commands
 import requests
 
-client = commands.Bot(intents=nextcord.Intents.all())
+client = nextcord.ext.commands.Bot(intents=nextcord.Intents.all())
 
 ###self#imports###############################################################################
 
-from cogs._global_data.global_data import network
+from cogs._global_data.GlobalData import network
 from database.database_command_uses import uses_update
 from database.database_lastfm import lastfm_get_user_from_db
 from utilities.maincommands import checks
 from utilities.variables import LASTFM_ICON, LASTFM_COLOR
 from utilities.partial_commands import get_nick_else_name, embed_builder
+from utilities.LastFmButtons import LastFmButtons
 
 
 
-class LastFmRecentButtons(nextcord.ui.View):
-    def __init__(self, page, last_page):
-        self.value = None
-        self.page: int = page
-        self.last_page: int = last_page
-        super().__init__(timeout = 60)
+async def lastfm_recent(interaction, member, lastfm_username, page_number, first_message_sent):
+    request_url = requests.get(f"http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&username={lastfm_username}&limit=10&page={page_number}&api_key={network.api_key}&format=json")
 
-    @nextcord.ui.button(label = "start", style=nextcord.ButtonStyle.green)
-    async def lf_recent_start(self,
-                              button: nextcord.ui.Button,
-                              interaction: Interaction):
-        self.page = 1
-        self.value = True
-        self.stop()
-
-    @nextcord.ui.button(label = "<<", style=nextcord.ButtonStyle.green)
-    async def lf_recent_left(self,
-                             button: nextcord.ui.Button,
-                             interaction: Interaction):
-        self.page -= 1
-        if self.page == 0:
-            self.page = 1
-        self.value = True
-        self.stop()
-
-    @nextcord.ui.button(label = ">>", style=nextcord.ButtonStyle.red)
-    async def lf_recent_right(self,
-                              button: nextcord.ui.Button,
-                              interaction: Interaction):
-        self.page += 1
-        if self.page > self.last_page:
-            self.page = self.last_page
-        self.value = True
-        self.stop()
-
-    @nextcord.ui.button(label = "end", style=nextcord.ButtonStyle.red)
-    async def lf_recent_end(self,
-                            button: nextcord.ui.Button,
-                            interaction: Interaction):
-        self.page = self.last_page
-        self.value = True
-        self.stop()
-
-    async def on_timeout(self):
-        for child in self.children:
-            child.disabled = True
-        await message.edit(view=self)
-
-
-
-async def lastfm_recent(interaction, member, page_number, first_message_sent):
-    lastfm_username = lastfm_get_user_from_db(member.id)
-
-    if not lastfm_username:
-        await interaction.response.send_message(f"{member.mention} has not setup their LastFm account.", ephemeral=True)
+    if not request_url.status_code == 200:
+        await interaction.followup.send("LastFm didn't respond correctly, try in a few minutes again!", ephemeral=True)
         return
 
-    request_url = f"http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&username={lastfm_username}&limit=10&page={page_number}&api_key={network.api_key}&format=json"
-
-    if not requests.get(request_url).status_code == 200:
-        await interaction.response.send_message(f"The user `{lastfm_username}` couldn't be found on LastFm.", ephemeral=True)
-        return
-
-    recent_user_data = requests.get(request_url).json()
-    lastpage = int(recent_user_data["recenttracks"]["@attr"]["totalPages"])
+    recent_user_data = request_url.json()
+    last_page = int(recent_user_data["recenttracks"]["@attr"]["totalPages"])
     name = get_nick_else_name(member)
     output = ""
     i = 0
@@ -91,11 +34,11 @@ async def lastfm_recent(interaction, member, page_number, first_message_sent):
         i += 1
         artist_name_for_url = track['artist']['#text'].replace(" ", "+")
 
-        try:
+        if "date" in track:
             timestamp = f"<t:{track['date']['uts']}:R>"
             output += f"{i + (page_number - 1) * 10}. **[{track['name']}]({track['url']})** by [{track['artist']['#text']}](https://www.last.fm/music/{artist_name_for_url}/) - {timestamp}\n"
 
-        except:
+        else:
             i -= 1
             if page_number == 1:
                 timestamp = "*now playing*"
@@ -105,29 +48,29 @@ async def lastfm_recent(interaction, member, page_number, first_message_sent):
                           color = LASTFM_COLOR,
                           author = f"{name} recently played:",
                           author_icon = LASTFM_ICON,
-                          footer = f"Page {page_number} of {lastpage}")
+                          footer = "DEFAULT_KST_FOOTER")
 
-    view = LastFmRecentButtons(page = page_number, last_page = lastpage)
-
-    global message
+    view = LastFmButtons(page = page_number, last_page = last_page, interaction = interaction)
 
     if not first_message_sent:
-        message = await interaction.send(embed=embed, view=view)
+        first_message_sent = True
+        await interaction.followup.send(embed=embed, view=view)
     else:
-        await first_message_sent.edit(embed=embed, view=view)
-        message = first_message_sent
+        await interaction.edit_original_message(embed=embed, view=view)
 
+    await view.change_page_button()
+    await view.check_page_for_button_deactivation()
     await view.wait()
 
     if view.value == None:
         uses_update("command_uses", "lf recent")
         return
 
-    await lastfm_recent(interaction, member, page_number = view.page, first_message_sent = message)
+    await lastfm_recent(interaction, member, lastfm_username, page_number = view.page, first_message_sent = first_message_sent)
 
 
 
-class LastFmRecent(commands.Cog):
+class LastFmRecent(nextcord.ext.commands.Cog):
 
     def __init__(self, client):
         self.client = client
@@ -136,12 +79,12 @@ class LastFmRecent(commands.Cog):
 
     ###lf#recent###########################################################
 
-    @lastfm.subcommand(name = "recent", description = "shows your recently played songs on LastFm")
+    @lastfm.subcommand(name = "rc", description = "shows your recently played songs on LastFm", name_localizations = {country_tag:"recent" for country_tag in nextcord.Locale})
     async def recent(self,
-                     interaction: Interaction,
+                     interaction: nextcord.Interaction,
                      *,
-                     member: nextcord.Member = SlashOption(description="the user you want the recent tracks of", required=False)):
-        if not checks(interaction):
+                     member: nextcord.Member = nextcord.SlashOption(description="the user you want the recent tracks of", required=False)):
+        if not checks(interaction.guild, interaction.user):
             return
 
         if member == None:
@@ -149,7 +92,15 @@ class LastFmRecent(commands.Cog):
 
         print(f"{interaction.user}: /lf recent {member}")
 
-        await lastfm_recent(interaction, member, page_number = 1, first_message_sent = False)
+        lastfm_username = lastfm_get_user_from_db(member.id)
+
+        if not lastfm_username:
+            await interaction.response.send_message(f"{member.mention} has not setup their LastFm account.", ephemeral=True)
+            return
+
+        await interaction.response.defer(with_message = True)
+
+        await lastfm_recent(interaction, member, lastfm_username, page_number = 1, first_message_sent = False)
 
 
 
--- cogs/lastfm/LastFmTopAlbums.py
+++ cogs/lastfm/LastFmTopAlbums.py
@@ -1,88 +1,31 @@
 ###package#import###############################################################################
 
 import nextcord
-from nextcord import Interaction, SlashOption
-from nextcord.ext import commands
 import requests
 
-client = commands.Bot(intents=nextcord.Intents.all())
+client = nextcord.ext.commands.Bot(intents=nextcord.Intents.all())
 
 ###self#imports###############################################################################
 
-from cogs._global_data.global_data import network
+from cogs._global_data.GlobalData import network
 from database.database_command_uses import uses_update
 from database.database_lastfm import lastfm_get_user_from_db
 from utilities.maincommands import checks
 from utilities.variables import LASTFM_ICON, LASTFM_COLOR
 from utilities.partial_commands import get_nick_else_name, embed_builder
+from utilities.LastFmButtons import LastFmButtons
 
 
 
-class LastFmTopAlbumsButtons(nextcord.ui.View):
-    def __init__(self, page, last_page):
-        self.value = None
-        self.page: int = page
-        self.last_page: int = last_page
-        super().__init__(timeout = 60)
+async def lastfm_top_albums(interaction, member, lastfm_username, timeframe, page_number, first_message_sent):
+    request_url = requests.get(f"http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&username={lastfm_username}&limit=10&page={page_number}&period={timeframe}&api_key={network.api_key}&format=json")
 
-    @nextcord.ui.button(label = "start", style=nextcord.ButtonStyle.green)
-    async def lf_top_albums_start(self,
-                                  button: nextcord.ui.Button,
-                                  interaction: Interaction):
-        self.page = 1
-        self.value = True
-        self.stop()
-
-    @nextcord.ui.button(label = "<<", style=nextcord.ButtonStyle.green)
-    async def lf_top_albums_left(self,
-                                 button: nextcord.ui.Button,
-                                 interaction: Interaction):
-        self.page -= 1
-        if self.page == 0:
-            self.page = 1
-        self.value = True
-        self.stop()
-
-    @nextcord.ui.button(label = ">>", style=nextcord.ButtonStyle.red)
-    async def lf_top_albums_right(self,
-                                  button: nextcord.ui.Button,
-                                  interaction: Interaction):
-        self.page += 1
-        if self.page > self.last_page:
-            self.page = self.last_page
-        self.value = True
-        self.stop()
-
-    @nextcord.ui.button(label = "end", style=nextcord.ButtonStyle.red)
-    async def lf_top_albums_end(self,
-                                button: nextcord.ui.Button,
-                                interaction: Interaction):
-        self.page = self.last_page
-        self.value = True
-        self.stop()
-
-    async def on_timeout(self):
-        for child in self.children:
-            child.disabled = True
-        await message.edit(view=self)
-
-
-
-async def lastfm_top_albums(interaction, member, timeframe, page_number, first_message_sent):
-    lastfm_username = lastfm_get_user_from_db(member.id)
-
-    if not lastfm_username:
-        await interaction.response.send_message(f"{member.mention} has not setup their LastFm account.", ephemeral=True)
+    if not request_url.status_code == 200:
+        await interaction.followup.send("LastFm didn't respond correctly, try in a few minutes again!", ephemeral=True)
         return
 
-    request_url = f"http://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&username={lastfm_username}&limit=10&page={page_number}&period={timeframe}&api_key={network.api_key}&format=json"
-
-    if not requests.get(request_url).status_code == 200:
-        await interaction.response.send_message(f"The user `{lastfm_username}` couldn't be found on LastFm.", ephemeral=True)
-        return
-
-    top_albums_user_data = requests.get(request_url).json()
-    lastpage = int(top_albums_user_data["topalbums"]["@attr"]["totalPages"])
+    top_albums_user_data = request_url.json()
+    last_page = int(top_albums_user_data["topalbums"]["@attr"]["totalPages"])
     name = get_nick_else_name(member)
     output = ""
 
@@ -96,29 +39,29 @@ async def lastfm_top_albums(interaction, member, timeframe, page_number, first_m
                           color = LASTFM_COLOR,
                           author = author_text,
                           author_icon = LASTFM_ICON,
-                          footer = f"Page {page_number} of {lastpage}")
+                          footer = "DEFAULT_KST_FOOTER")
 
-    view = LastFmTopAlbumsButtons(page = page_number, last_page = lastpage)
-
-    global message
+    view = LastFmButtons(page = page_number, last_page = last_page, interaction = interaction)
 
     if not first_message_sent:
-        message = await interaction.send(embed=embed, view=view)
+        first_message_sent = True
+        await interaction.followup.send(embed=embed, view=view)
     else:
-        await first_message_sent.edit(embed=embed, view=view)
-        message = first_message_sent
+        await interaction.edit_original_message(embed=embed, view=view)
 
+    await view.change_page_button()
+    await view.check_page_for_button_deactivation()
     await view.wait()
 
     if view.value == None:
         uses_update("command_uses", "lf topalbums")
         return
 
-    await lastfm_top_albums(interaction, member, timeframe, page_number = view.page, first_message_sent = message)
+    await lastfm_top_albums(interaction, member, lastfm_username, timeframe, page_number = view.page, first_message_sent = first_message_sent)
 
 
 
-class LastFmTopAlbums(commands.Cog):
+class LastFmTopAlbums(nextcord.ext.commands.Cog):
 
     def __init__(self, client):
         self.client = client
@@ -129,11 +72,11 @@ class LastFmTopAlbums(commands.Cog):
 
     @lastfm.subcommand(name = "topalbums", description = "shows your top albums on LastFm")
     async def top_albums(self,
-                         interaction: Interaction,
+                         interaction: nextcord.Interaction,
                          *,
-                         member: nextcord.Member = SlashOption(description="the user you want the top albums of", required=False),
-                         timeframe: str = SlashOption(description="the timeframe you want the top albums for", required=False, choices = {"Past Week": "7day", "Past Month": "1month", "Past Quarter": "3month", "Past Half a Year": "6month", "Past Year": "12month", "All Time": "overall"})):
-        if not checks(interaction):
+                         member: nextcord.Member = nextcord.SlashOption(description="the user you want the top albums of", required=False),
+                         timeframe: str = nextcord.SlashOption(description="the timeframe you want the top albums for", required=False, choices = {"Past Week": "7day", "Past Month": "1month", "Past Quarter": "3month", "Past Half a Year": "6month", "Past Year": "12month", "All Time": "overall"})):
+        if not checks(interaction.guild, interaction.user):
             return
 
         if member == None:
@@ -144,7 +87,15 @@ class LastFmTopAlbums(commands.Cog):
 
         print(f"{interaction.user}: /lf topalbums {member} {timeframe}")
 
-        await lastfm_top_albums(interaction, member, timeframe, page_number = 1, first_message_sent = False)
+        lastfm_username = lastfm_get_user_from_db(member.id)
+
+        if not lastfm_username:
+            await interaction.response.send_message(f"{member.mention} has not setup their LastFm account.", ephemeral=True)
+            return
+
+        await interaction.response.defer(with_message = True)
+
+        await lastfm_top_albums(interaction, member, lastfm_username, timeframe, page_number = 1, first_message_sent = False)
 
 
 

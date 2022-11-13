c8b4986f948e87a434c578b4a2e3ddceb7b8250e
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-11-13 17:46:58 +0100

    removed /si and /ui changed time coversions to be local and easier

--- cogs/info/Serverinfo.py
+++ cogs/info/Serverinfo.py
@@ -1,55 +1,73 @@
 ###package#import###############################################################################
 
 import nextcord
-from nextcord import Interaction
-from nextcord.ext import commands
+import time
 
-client = commands.Bot(intents=nextcord.Intents.all())
+client = nextcord.ext.commands.Bot(intents=nextcord.Intents.all())
 
 ###self#imports###############################################################################
 
 from database.database_command_uses import uses_update
 from utilities.maincommands import checks
-from utilities.partial_commands import get_serverinfo_embed
+from utilities.partial_commands import embed_builder
+from utilities.variables import DEFAULT_PFP, BOT_COLOR
 
 
 
-class severinfo(commands.Cog):
+class Severinfo(nextcord.ext.commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
     ###serverinfo###########################################################
         
-    @nextcord.slash_command(name="serverinfo", description = "gives information about this server")
+    @nextcord.slash_command(name = "si", description = "gives information about this server", name_localizations = {country_tag:"serverinfo" for country_tag in nextcord.Locale})
     async def serverinfo(self,
-                         interaction: Interaction):
-        if not checks(interaction):
+                         interaction: nextcord.Interaction):
+        if not checks(interaction.guild, interaction.user):
             return
 
         print(f"{interaction.user}: /serverinfo")
 
-        embed = await get_serverinfo_embed(self.client, interaction)
+        created_time = int(time.mktime(interaction.guild.created_at.timetuple()))
 
-        await interaction.send(embed=embed)
+        guild_with_counts = await client.fetch_guild(interaction.guild.id, with_counts=True)
+
+        if interaction.guild.icon is not None:
+            server_icon_url = interaction.guild.icon
+        else:
+            server_icon_url = DEFAULT_PFP
+
+        embed = embed_builder(title = f"Server Information: `{interaction.guild.name}`",
+                            color = BOT_COLOR,
+                            thumbnail = server_icon_url,
+                            footer = "DEFAULT_KST_FOOTER",
+
+                            field_one_name = "ID:",
+                            field_one_value = interaction.guild.id,
+                            field_one_inline = False,
+                                                
+                            field_two_name = "Owner:",
+                            field_two_value = interaction.guild.owner.mention,
+                            field_two_inline = True,
+                                                
+                            field_three_name = "Members:",
+                            field_three_value = f"Total: {interaction.guild.member_count}\nOnline: {guild_with_counts.approximate_presence_count}",
+                            field_three_inline = True,
+                                                
+                            field_four_name = "Channels:",
+                            field_four_value = f"Text: {len(interaction.guild.text_channels)}\nVoice: {len(interaction.guild.voice_channels)}",
+                            field_four_inline = True,
+                                                
+                            field_five_name = "Created at:",
+                            field_five_value = f"<t:{created_time}>",
+                            field_five_inline = True)
+
+        await interaction.response.send_message(embed=embed)
 
         uses_update("command_uses", "serverinfo")
 
-    ###serverinfo#alias###########################################################
 
-    @nextcord.slash_command(name="si", description = "gives information about this server (alias of /serverinfo)")
-    async def si(self,
-                 interaction: Interaction):
-        if not checks(interaction):
-            return
-
-        print(f"{interaction.user}: /si")
-
-        embed = await get_serverinfo_embed(self.client, interaction)
-
-        await interaction.send(embed=embed)
-
-        uses_update("command_uses", "si")
 
 def setup(client):
-    client.add_cog(severinfo(client))
+    client.add_cog(Severinfo(client))
--- cogs/info/Userinfo.py
+++ cogs/info/Userinfo.py
@@ -1,32 +1,32 @@
 ###package#import###############################################################################
 
 import nextcord
-from nextcord import Interaction, SlashOption
-from nextcord.ext import commands
+import time
 
-client = commands.Bot(intents=nextcord.Intents.all())
+client = nextcord.ext.commands.Bot(intents=nextcord.Intents.all())
 
 ###self#imports###############################################################################
 
 from database.database_command_uses import uses_update
 from utilities.maincommands import checks
-from utilities.partial_commands import get_userinfo_embed
+from utilities.partial_commands import get_nick_else_name, get_user_avatar, embed_builder
+from utilities.variables import SKILLP_ID, SERVER_ID, SKILLP_JOINED_UNIX_TIME
 
 
 
-class userinfo(commands.Cog):
+class Userinfo(nextcord.ext.commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
     ###userinfo###########################################################
 
-    @nextcord.slash_command(name="userinfo", description = "gives information about a user")
+    @nextcord.slash_command(name = "ui", description = "gives information about a user", name_localizations = {country_tag:"userinfo" for country_tag in nextcord.Locale})
     async def userinfo(self,
-                       interaction: Interaction,
+                       interaction: nextcord.Interaction,
                        *,
-                       member: nextcord.Member = SlashOption(description="the user you want the information about (nothing=yourself)", required=False)):
-        if not checks(interaction):
+                       member: nextcord.Member = nextcord.SlashOption(description="the user you want information about", required=False)):
+        if not checks(interaction.guild, interaction.user):
             return
 
         print(f"{interaction.user}: /userinfo {member}")
@@ -34,32 +34,53 @@ class userinfo(commands.Cog):
         if member == None:
             member = interaction.guild.get_member(interaction.user.id)
 
-        embed = get_userinfo_embed(member)
+        created_time = int(time.mktime(member.created_at.timetuple()))
+        name = get_nick_else_name(member)
+        member_avatar_url = get_user_avatar(member)
 
-        await interaction.send(embed=embed)
+        if member.id == SKILLP_ID and member.guild.id == SERVER_ID:
+            joined_time = SKILLP_JOINED_UNIX_TIME
+        else:
+            joined_time = int(time.mktime(member.joined_at.timetuple()))
+
+        embed = embed_builder(title = f"User Information: `{name}`",
+                              color = member.color,
+                              thumbnail = member_avatar_url,
+                              footer = "DEFAULT_KST_FOOTER",
+
+                              field_one_name = "ID:",
+                              field_one_value = member.id,
+                              field_one_inline = False,
+                                                
+                              field_two_name = "Name:",
+                              field_two_value = member,
+                              field_two_inline = True,
+                                                
+                              field_three_name = "Top role:",
+                              field_three_value = member.top_role.mention,
+                              field_three_inline = True,
+                                                
+                              field_four_name = "Status:",
+                              field_four_value = member.status,
+                              field_four_inline = True,
+                                                
+                              field_five_name = "Created at:",
+                              field_five_value = f"<t:{created_time}>",
+                              field_five_inline = True,
+                                                
+                              field_six_name = "Joined at:",
+                              field_six_value = f"<t:{joined_time}>",
+                              field_six_inline = True,
+
+                              field_seven_name = "Boosted:",
+                              field_seven_value = bool(member.premium_since),
+                              field_seven_inline = True)
+
+        await interaction.response.send_message(embed=embed)
 
         uses_update("command_uses", "userinfo")
 
-    ###userinfo#alias###########################################################
 
-    @nextcord.slash_command(name="ui", description = "gives information about a user (alias of /userinfo)")
-    async def ui(self,
-                 interaction: Interaction,
-                 *,
-                 member: nextcord.Member = SlashOption(description="the user you want the information about (nothing=yourself)", required=False)):
-        if not checks(interaction):
-            return
-
-        print(f"{interaction.user}: /ui {member}")
-
-        if member == None:
-            member = interaction.guild.get_member(interaction.user.id)
-
-        embed = get_userinfo_embed(member)
-
-        await interaction.send(embed=embed)
-
-        uses_update("command_uses", "ui")
 
 def setup(client):
-    client.add_cog(userinfo(client))
+    client.add_cog(Userinfo(client))

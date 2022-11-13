6bbc70b65b90ec8e9398cf338567dd435e947712
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-11-13 18:10:14 +0100

    made interaction defer

--- cogs/simple/Wolfram.py
+++ cogs/simple/Wolfram.py
@@ -1,15 +1,13 @@
 ###package#import###############################################################################
 
+import dotenv
 import nextcord
-from nextcord import Interaction, SlashOption
-from nextcord.ext import commands
 import os
 import wolframalpha
-from dotenv import load_dotenv
 
-load_dotenv()
+dotenv.load_dotenv()
 
-client = commands.Bot(intents=nextcord.Intents.all())
+client = nextcord.ext.commands.Bot(intents=nextcord.Intents.all())
 
 ###self#imports###############################################################################
 
@@ -18,7 +16,7 @@ from utilities.maincommands import checks
 
 
 
-class wolfram(commands.Cog):
+class Wolfram(nextcord.ext.commands.Cog):
 
     def __init__(self, client):
         self.client = client
@@ -27,29 +25,29 @@ class wolfram(commands.Cog):
 
     @nextcord.slash_command(name = "wolfram", description = "find an answer to a query")
     async def wolfram(self,
-                      interaction: Interaction,
+                      interaction: nextcord.Interaction,
                       *,
-                      query: str = SlashOption(description="for what do you want an answer", required=True, min_length=1, max_length=200)):
-        if not checks(interaction):
+                      query: str = nextcord.SlashOption(description="for what do you want an answer", required=True, min_length=1, max_length=200)):
+        if not checks(interaction.guild, interaction.user):
             return
 
         print(f"{interaction.user}: /wolfram {query}")
 
-        response = await interaction.response.send_message(f"Gathering answer...")
+        await interaction.response.defer(with_message = True)
 
         client = wolframalpha.Client(os.getenv("WOLFRAM_APP_ID"))
 
         query_result = client.query(query)
 
         try:
-            await response.edit(f"Query: `{query}`\n```{next(query_result.results).text}```")
+            await interaction.followup.send(f"Query: `{query}`\n```{next(query_result.results).text}```")
 
         except:
-            await response.edit(f"Wolfram couldn't find a result for your query:\n`{query}`")
+            await interaction.followup.send(f"Wolfram couldn't find a result for your query:\n`{query}`")
 
         uses_update("command_uses", "wolfram")
 
 
 
 def setup(client):
-    client.add_cog(wolfram(client))
+    client.add_cog(Wolfram(client))

6d745fa6aec487fba09d34dbbb2db071ac32a1f3
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-11-13 18:08:47 +0100

    added list comprehension for Options

--- cogs/simple/Choose.py
+++ cogs/simple/Choose.py
@@ -1,11 +1,9 @@
 ###package#import###############################################################################
 
 import nextcord
-from nextcord import Interaction, SlashOption
-from nextcord.ext import commands
 import random
 
-client = commands.Bot(intents=nextcord.Intents.all())
+client = nextcord.ext.commands.Bot(intents=nextcord.Intents.all())
 
 ###self#imports###############################################################################
 
@@ -14,7 +12,7 @@ from utilities.maincommands import checks
 
 
 
-class choose(commands.Cog):
+class Choose(nextcord.ext.commands.Cog):
 
     def __init__(self, client):
         self.client = client
@@ -23,34 +21,27 @@ class choose(commands.Cog):
     
     @nextcord.slash_command(name = "choose", description = "let the bot choose one of the options for you!")
     async def choose(self,
-                     interaction: Interaction,
+                     interaction: nextcord.Interaction,
                      *,
-                     option1: str = SlashOption(description="first Option", required=True, min_length=1, max_length=200),
-                     option2: str = SlashOption(description="second Option", required=True, min_length=1, max_length=200),
-                     option3: str = SlashOption(description="third Option", required=False, min_length=1, max_length=200),
-                     option4: str = SlashOption(description="fourth Option", required=False, min_length=1, max_length=200),
-                     option5: str = SlashOption(description="fifth Option", required=False, min_length=1, max_length=200),
-                     option6: str = SlashOption(description="sixth Option", required=False, min_length=1, max_length=200),
-                     option7: str = SlashOption(description="seventh Option", required=False, min_length=1, max_length=200),
-                     option8: str = SlashOption(description="eigth Option", required=False, min_length=1, max_length=200),
-                     option9: str = SlashOption(description="ninth Option", required=False, min_length=1, max_length=200),
-                     option10: str = SlashOption(description="tenth Option", required=False, min_length=1, max_length=200)):
-        if not checks(interaction):
+                     option1: str = nextcord.SlashOption(description="first option", required=True, min_length=1, max_length=200),
+                     option2: str = nextcord.SlashOption(description="second option", required=True, min_length=1, max_length=200),
+                     option3: str = nextcord.SlashOption(description="third option", required=False, min_length=1, max_length=200),
+                     option4: str = nextcord.SlashOption(description="fourth option", required=False, min_length=1, max_length=200),
+                     option5: str = nextcord.SlashOption(description="fifth option", required=False, min_length=1, max_length=200),
+                     option6: str = nextcord.SlashOption(description="sixth option", required=False, min_length=1, max_length=200),
+                     option7: str = nextcord.SlashOption(description="seventh option", required=False, min_length=1, max_length=200),
+                     option8: str = nextcord.SlashOption(description="eigth option", required=False, min_length=1, max_length=200),
+                     option9: str = nextcord.SlashOption(description="ninth option", required=False, min_length=1, max_length=200),
+                     option10: str = nextcord.SlashOption(description="tenth option", required=False, min_length=1, max_length=200)):
+        if not checks(interaction.guild, interaction.user):
             return
 
         RawOptions = []
-        Options = []
-        i = 0
-
         RawOptions.extend(value for name, value in locals().items() if name.startswith('option'))
 
         print(f"{interaction.user}: /choose {RawOptions}")
 
-        while i < len(RawOptions):
-            if RawOptions[i] != None:
-                Options.append(RawOptions[i])
-            
-            i += 1
+        Options = [option for option in RawOptions if option != None]
 
         chosen_option_int = random.randint(0, len(Options)-1)
         chosen_option = Options[chosen_option_int]
@@ -59,5 +50,7 @@ class choose(commands.Cog):
 
         uses_update("command_uses", "choose")
 
+
+
 def setup(client):
-    client.add_cog(choose(client))
+    client.add_cog(Choose(client))

398b89b324f4a118c41c1ebb2f94c354777f5dfc
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-11-13 17:44:27 +0100

    removed /cl, used localization to have cl as a name and commandlist as a displayname

--- cogs/command_overview/Commandlist.py
+++ cogs/command_overview/Commandlist.py
@@ -1,10 +1,8 @@
 ###package#import###############################################################################
 
 import nextcord
-from nextcord import Interaction
-from nextcord.ext import commands
 
-client = commands.Bot(intents=nextcord.Intents.all())
+client = nextcord.ext.commands.Bot(intents=nextcord.Intents.all())
 
 ###self#imports###############################################################################
 
@@ -15,17 +13,17 @@ from utilities.partial_commands import embed_builder
 
 
 
-class commandlist(commands.Cog):
+class Commandlist(nextcord.ext.commands.Cog):
 
     def __init__(self, client):
         self.client = client
 
     ###commandlist###########################################################
 
-    @nextcord.slash_command(name = "commandlist", description = "a list of all main commands")
+    @nextcord.slash_command(name = "cl", description = "a list of all main commands", name_localizations = {country_tag:"commandlist" for country_tag in nextcord.Locale})
     async def commandlist(self,
-                         interaction: Interaction):
-        if not checks(interaction):
+                          interaction: nextcord.Interaction):
+        if not checks(interaction.guild, interaction.user):
             return
 
         print(f"{interaction.user}: /commandlist")
@@ -46,31 +44,7 @@ class commandlist(commands.Cog):
 
         uses_update("command_uses", "commandlist")
 
-    ###commandlist#alias###########################################################
 
-    @nextcord.slash_command(name = "cl", description = "a list of all main commands (alias of /commandlist)")
-    async def cl(self,
-                 interaction: Interaction):
-        if not checks(interaction):
-            return
-
-        print(f"{interaction.user}: /cl")
-
-        embed = embed_builder(title = "A list of all main commands",
-                              color = BOT_COLOR,
-                              footer = "DEFAULT_KST_FOOTER",
-
-                              field_one_name = "Commands:",
-                              field_one_value = COMMAND_LIST,
-                              field_one_inline = True,
-
-                              field_two_name = "Help:",
-                              field_two_value = "Explanations for singular commands can be found by typing `/help`!",
-                              field_two_inline = False)
-        
-        await interaction.send(embed=embed, ephemeral=True)
-
-        uses_update("command_uses", "cl")
 
 def setup(client):
-    client.add_cog(commandlist(client))
+    client.add_cog(Commandlist(client))

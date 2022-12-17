d52751891299a0d0081ef6b8e56622bfefa0e8d6
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-12-18 00:49:03 +0100

    rewrote custom db

--- lib/db_modules/CustomDB.py
+++ lib/db_modules/CustomDB.py
@@ -1,124 +1,131 @@
-import sqlite3
+####################################################################################################
+
 import os
+import sqlite3
 
-###custom#add###########################################################
+####################################################################################################
 
-def create_custom_command(server_id, clean_commandname, commandtext):
-    database_path = os.path.join(os.path.dirname(__file__), '../storage/db/custom_commands.db')
-    conn = sqlite3.connect(database_path)
 
-    c = conn.cursor()
 
-    c.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='server{server_id}'")
+class CustomDB():
 
-    if c.fetchone() != None :
-        pass
-    else:
-        c.execute(f"CREATE TABLE server{server_id} (clean_commandname text, commandtext text)")
+    def __init__(self) -> None:
+        self.database_path = os.path.join(os.path.dirname(__file__), '../../storage/db/custom_commands.db')
+
+    ####################################################################################################
+
+    def create_table(self,
+                     server_id: int) -> None:
+        """Creates a table, if there isn't one already"""
+
+        conn = sqlite3.connect(self.database_path)
+        c = conn.cursor()
+
+        c.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='server{server_id}'")
+
+        if not c.fetchone():
+            c.execute(f"""CREATE TABLE server{server_id} (clean_commandname text,
+                                                          commandtext text)""")
+
+            conn.commit()
+
+        conn.close()
+
+    ####################################################################################################
+
+    def create_command(self,
+                       server_id: int,
+                       clean_commandname: str,
+                       commandtext: str) -> bool:
+        """This function adds a custom command to the server, if there isn't already a custom command with the same name"""
+
+        self.create_table(server_id)
+
+        conn = sqlite3.connect(self.database_path)
+        c = conn.cursor()
+
+        c.execute(f"SELECT clean_commandname FROM server{server_id} WHERE clean_commandname = '{clean_commandname}'")
+        
+        if c.fetchone():
+            conn.close()
+            return False
+
+        c.execute(f"INSERT INTO server{server_id} VALUES ('{clean_commandname}', '{commandtext}')")
 
         conn.commit()
 
-    c.execute(f"SELECT clean_commandname FROM server{server_id}")
-    tuple_all_commandnames = c.fetchall()
-    all_commandnames = []
-
-    for commandname in tuple_all_commandnames:
-        all_commandnames.append(commandname[0])
-
-    if clean_commandname in all_commandnames:
         conn.close()
-        return False
+        return True
 
-    c.execute(f"INSERT INTO server{server_id} VALUES ('{clean_commandname}', '{commandtext}')")
+    ####################################################################################################
 
-    conn.commit()
+    def delete_command(self, 
+                       server_id: int,
+                       clean_commandname: str) -> str:
+        """This function will delete a custom command, if the command was deleted it will return the commandtext, otherwise an empty string"""
 
-    conn.close()
-    return True
+        self.create_table(server_id)
 
-###custom#delete###########################################################
+        conn = sqlite3.connect(self.database_path)
+        c = conn.cursor()
 
-def delete_custom_command(server_id, clean_commandname):
-    database_path = os.path.join(os.path.dirname(__file__), '../storage/db/custom_commands.db')
-    conn = sqlite3.connect(database_path)
+        c.execute(f"SELECT clean_commandname FROM server{server_id} WHERE clean_commandname = '{clean_commandname}'")
 
-    c = conn.cursor()
+        if not c.fetchone():
+            conn.close()
+            return ""
 
-    c.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='server{server_id}'")
+        c.execute(f"SELECT commandtext FROM server{server_id} WHERE clean_commandname = '{clean_commandname}'")
 
-    if c.fetchone() == None:
+        commandtext = c.fetchone()[0]
+
+        c.execute(f"DELETE from server{server_id} WHERE clean_commandname = '{clean_commandname}'")
+
+        conn.commit()
+
         conn.close()
-        return ""
+        return commandtext
 
-    c.execute(f"SELECT clean_commandname FROM server{server_id}")
-    tuple_all_commandnames = c.fetchall()
-    all_commandnames = []
+    ####################################################################################################
 
-    for commandname in tuple_all_commandnames:
-        all_commandnames.append(commandname[0])
+    def list(self,
+             server_id: int) -> list | list[str]:
+        """This command returns a list of all custom commands a server has. If the server has no custom commands it will retrun an empty list"""
 
-    if not clean_commandname in all_commandnames:
+        self.create_table(server_id)
+
+        conn = sqlite3.connect(self.database_path)
+        c = conn.cursor()
+
+        c.execute(f"SELECT clean_commandname FROM server{server_id} ORDER BY clean_commandname ASC")
+
+        tuple_all_commandnames = c.fetchall()
+        all_commandnames = [commandname[0] for commandname in tuple_all_commandnames]
+
         conn.close()
-        return ""
+        return all_commandnames
 
-    c.execute(f"SELECT commandtext FROM server{server_id} WHERE clean_commandname = '{clean_commandname}'")
-    tuple_commandtext = c.fetchall()
-    commandtext = ""
+    ####################################################################################################
 
-    for command_text in tuple_commandtext:
-        commandtext += command_text[0]
+    def get_command_text(self,
+                         server_id: int,
+                         clean_commandname: str) -> str:
+        """This function will get the command text of a custom command, if the input custom command doesn't exist it will return an empty string"""
 
-    c.execute(f"DELETE from server{server_id} WHERE clean_commandname = '{clean_commandname}'")
+        self.create_table(server_id)
 
-    conn.commit()
+        conn = sqlite3.connect(self.database_path)
+        c = conn.cursor()
 
-    conn.close()
-    return commandtext
+        c.execute(f"SELECT clean_commandname FROM server{server_id} WHERE clean_commandname = '{clean_commandname}'")
 
-###custom#list###########################################################
+        if not c.fetchone():
+            conn.close()
+            return ""
 
-def list_custom(server_id):
-    database_path = os.path.join(os.path.dirname(__file__), '../storage/db/custom_commands.db')
-    conn = sqlite3.connect(database_path)
+        c.execute(f"SELECT commandtext FROM server{server_id} WHERE clean_commandname = '{clean_commandname}'")
 
-    c = conn.cursor()
+        commandtext = c.fetchone()[0]
 
-    c.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='server{server_id}'")
-
-    if c.fetchone() == None :
         conn.close()
-        return []
-
-    c.execute(f"SELECT clean_commandname FROM server{server_id} ORDER BY clean_commandname ASC")
-    tuple_all_commandnames = c.fetchall()
-    all_commandnames = []
-
-    for commandname in tuple_all_commandnames:
-        all_commandnames.append(commandname[0])
-
-    conn.close()
-    return all_commandnames
-
-###custom#command###########################################################
-
-def command_custom(server_id, clean_commandname):
-    database_path = os.path.join(os.path.dirname(__file__), '../storage/db/custom_commands.db')
-    conn = sqlite3.connect(database_path)
-
-    c = conn.cursor()
-
-    c.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='server{server_id}'")
-
-    if c.fetchone() == None :
-        conn.close()
-        return ""
-
-    c.execute(f"SELECT commandtext FROM server{server_id} WHERE clean_commandname = '{clean_commandname}'")
-    tuple_commandtext = c.fetchall()
-    commandtext = ""
-
-    for command_text in tuple_commandtext:
-        commandtext = command_text[0]
-
-    conn.close()
-    return commandtext
+        return commandtext

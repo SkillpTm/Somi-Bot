204d77e1fb61ffc2fc3fe9b0b5f273dde0fca042
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-12-18 00:42:14 +0100

    rewrote /lf set

--- cogs/lastfm/LastFmSet.py
+++ cogs/lastfm/LastFmSet.py
@@ -1,84 +1,78 @@
-###package#import###############################################################################
+####################################################################################################
 
 import nextcord
+import nextcord.ext.commands as nextcord_C
+import nextcord.ext.application_checks as nextcord_AC
 import requests
 
-client = nextcord.ext.commands.Bot(intents=nextcord.Intents.all())
+####################################################################################################
 
-###self#imports###############################################################################
-
-from cogs._global_data.GlobalData import network
-from database.database_command_uses import uses_update
-from database.database_lastfm import lastfm_get_user_from_db, lastfm_set_user, lastfm_reset_user
-from utilities.maincommands import checks
+from lib.db_modules import LastFmDB
+from lib.modules import Checks, EmbedFunctions
+from lib.utilities import SomiBot
 
 
 
-class LastFmSet(nextcord.ext.commands.Cog):
+class LastFmSet(nextcord_C.Cog):
 
     def __init__(self, client):
-        self.client = client
+        self.client: SomiBot = client
 
-    from utilities.maincommands import lastfm
+    from lib.utilities.main_commands import lastfm
 
-    ###lf#set###########################################################
+    ####################################################################################################
 
     @lastfm.subcommand(name = "set", description = "set your LastFm account")
-    async def set(self,
-                  interaction: nextcord.Interaction,
-                  *,
-                  lastfmname: str = nextcord.SlashOption(description="input your LastFm name", required=True, min_length=1, max_length=100)):
-        if not checks(interaction.guild, interaction.user):
-            return
+    @nextcord_AC.check(Checks().interaction_in_guild())
+    async def lastfm_set(self,
+                         interaction: nextcord.Interaction,
+                         *,
+                         lastfmname: str = nextcord.SlashOption(description="input your LastFm name", required=True, min_length=2, max_length=100)):
+        """This command connects a discord user and a lastfm account in the db"""
 
-        print(f"{interaction.user}: /lf set {lastfmname}")
+        self.client.Loggers.action_log(f"Guild: {interaction.guild.id} ~ Channel: {interaction.channel.id} ~ User: {interaction.user.id} ~ /lf set {lastfmname}")
 
-        lastfm_username = lastfm_get_user_from_db(interaction.user.id)
+        await interaction.response.defer(ephemeral=True, with_message = True)
 
-        await interaction.response.defer(with_message = True)
+        lastfm_username = LastFmDB().get_user(interaction.user.id)
 
         if lastfm_username:
-            await interaction.followup.send(f"You already set a LastFm account. To reset it use `/lf reset`.", ephemeral=True)
+            await interaction.followup.send(embed=EmbedFunctions().error(f"You already set a LastFm account.\nTo reset your LastFm account use `/lf reset`."), ephemeral=True)
             return
 
-        request_url = requests.get(f"http://ws.audioscrobbler.com/2.0/?method=user.getinfo&username={lastfmname}&api_key={network.api_key}&format=json")
+        request_url = requests.get(f"http://ws.audioscrobbler.com/2.0/?method=user.getinfo&username={lastfmname}&api_key={self.client.Keychain.LAST_FM_API_KEY}&format=json")
 
         if not request_url.status_code == 200:
-            await interaction.followup.send(f"The user `{lastfmname}` couldn't be found on LastFm.", ephemeral=True)
+            await interaction.followup.send(embed=EmbedFunctions().error(f"The user `{lastfmname}` couldn't be found on LastFm."), ephemeral=True)
             return
 
         recent_user_data = request_url.json()
-        lastfm_username = recent_user_data["user"]["name"]
 
-        lastfm_set_user(interaction.user.id, lastfm_username)
+        LastFmDB().set_user(interaction.user.id, recent_user_data["user"]["name"])
 
-        await interaction.followup.send(f"You were succesfully connected with the LastFm user `{lastfm_username}`", ephemeral=True)
+        await interaction.followup.send(embed=EmbedFunctions().success(f"You were succesfully connected with the LastFm user `{recent_user_data['user']['name']}`"), ephemeral=True)
 
-        uses_update("command_uses", "lf set")
-
-    ###lf#reset###########################################################
+    ####################################################################################################
 
     @lastfm.subcommand(name = "reset", description = "reset your LastFm-Discord account connection")
-    async def reset(self,
-                    interaction: nextcord.Interaction):
-        if not checks(interaction.guild, interaction.user):
-            return
+    @nextcord_AC.check(Checks().interaction_in_guild())
+    async def lastfm_reset(self,
+                           interaction: nextcord.Interaction):
+        """This command deletes the user's connection from the db"""
 
-        print(f"{interaction.user}: /lf reset")
+        self.client.Loggers.action_log(f"Guild: {interaction.guild.id} ~ Channel: {interaction.channel.id} ~ User: {interaction.user.id} ~ /lf reset")
 
-        lastfm_username = lastfm_get_user_from_db(interaction.user.id)
+        lastfm_username = LastFmDB().get_user(interaction.user.id)
 
         if not lastfm_username:
-            await interaction.response.send_message("You don't have a LastFm account connected to your discord account.", ephemeral=True)
+            await interaction.response.send_message(embed=EmbedFunctions().error("You don't have a LastFm account setup."), ephemeral=True)
             return
 
-        lastfm_reset_user(interaction.user.id)
+        LastFmDB().reset_user(interaction.user.id)
 
-        await interaction.response.send_message("You succesfully reset your LastFm-Discord account connection.", ephemeral=True)
-
-        uses_update("command_uses", "lf reset")
+        await interaction.response.send_message(embed=EmbedFunctions().success("You succesfully reset your LastFm account."), ephemeral=True)
 
 
 
-def setup(client):
+def setup(client: SomiBot):
     client.add_cog(LastFmSet(client))

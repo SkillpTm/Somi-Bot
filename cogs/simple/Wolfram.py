72d5c1925767bbbf3428d20297cc0f2298e0407e
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-12-18 00:47:58 +0100

    rewrote /wolfram

--- cogs/simple/Wolfram.py
+++ cogs/simple/Wolfram.py
@@ -1,53 +1,46 @@
-###package#import###############################################################################
+####################################################################################################
 
-import dotenv
 import nextcord
-import os
+import nextcord.ext.commands as nextcord_C
+import nextcord.ext.application_checks as nextcord_AC
 import wolframalpha
 
-dotenv.load_dotenv()
+####################################################################################################
 
-client = nextcord.ext.commands.Bot(intents=nextcord.Intents.all())
-
-###self#imports###############################################################################
-
-from database.database_command_uses import uses_update
-from utilities.maincommands import checks
+from lib.modules import Checks, EmbedFunctions
+from lib.utilities import SomiBot
 
 
 
-class Wolfram(nextcord.ext.commands.Cog):
+class Wolfram(nextcord_C.Cog):
 
     def __init__(self, client):
-        self.client = client
+        self.client: SomiBot = client
 
-    ###wolfram###########################################################
+    ####################################################################################################
 
     @nextcord.slash_command(name = "wolfram", description = "find an answer to a query")
+    @nextcord_AC.check(Checks().interaction_in_guild())
     async def wolfram(self,
                       interaction: nextcord.Interaction,
                       *,
-                      query: str = nextcord.SlashOption(description="for what do you want an answer", required=True, min_length=1, max_length=200)):
-        if not checks(interaction.guild, interaction.user):
-            return
+                      query: str = nextcord.SlashOption(description="for what do you want an answer", required=True, min_length=2, max_length=200)):
+        """This command uses the given query and sends it to the wolfram API, if a short answer can be found, it will be responded with"""
 
-        print(f"{interaction.user}: /wolfram {query}")
+        self.client.Loggers.action_log(f"Guild: {interaction.guild.id} ~ Channel: {interaction.channel.id} ~ User: {interaction.user.id} ~ /wolfram {query}")
 
         await interaction.response.defer(with_message = True)
 
-        client = wolframalpha.Client(os.getenv("WOLFRAM_APP_ID"))
+        wolfram_client = wolframalpha.Client(self.client.Keychain.WOLFRAM_APP_ID)
 
-        query_result = client.query(query)
+        query_result = wolfram_client.query(query)
 
         try:
             await interaction.followup.send(f"Query: `{query}`\n```{next(query_result.results).text}```")
-
         except:
-            await interaction.followup.send(f"Wolfram couldn't find a result for your query:\n`{query}`")
-
-        uses_update("command_uses", "wolfram")
+            await interaction.followup.send(embed=EmbedFunctions().error(f"Wolfram couldn't find a result for your query:\n`{query}`"))
 
 
 
-def setup(client):
+def setup(client: SomiBot):
     client.add_cog(Wolfram(client))

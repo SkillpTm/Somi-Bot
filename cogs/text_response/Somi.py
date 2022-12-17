37f07a07b8cb0c9a973aedf4385720f799777d83
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-12-18 00:48:12 +0100

    rewrote /somi

--- cogs/text_response/Somi.py
+++ cogs/text_response/Somi.py
@@ -1,37 +1,34 @@
-###package#import###############################################################################
+####################################################################################################
 
 import nextcord
+import nextcord.ext.commands as nextcord_C
+import nextcord.ext.application_checks as nextcord_AC
 
-client = nextcord.ext.commands.Bot(intents=nextcord.Intents.all())
+####################################################################################################
 
-###self#imports###############################################################################
-
-from database.database_command_uses import uses_update
-from utilities.maincommands import checks
-from utilities.variables import SOMI_BEST_GRILL_IMAGE
-
+from lib.modules import Checks
+from lib.utilities import SomiBot
 
 
-class Somi(nextcord.ext.commands.Cog):
+
+class Somi(nextcord_C.Cog):
 
     def __init__(self, client):
-        self.client = client
+        self.client: SomiBot = client
 
-    ###somi###########################################################
+    ####################################################################################################
 
     @nextcord.slash_command(name = "somi", description = "speaks facts")
+    @nextcord_AC.check(Checks().interaction_in_guild())
     async def somi(self,
                    interaction: nextcord.Interaction):
-        if not checks(interaction.guild, interaction.user):
-            return
+        """This command tells you the truth"""
 
-        print(f"{interaction.user}: /somi")
+        self.client.Loggers.action_log(f"Guild: {interaction.guild.id} ~ Channel: {interaction.channel.id} ~ User: {interaction.user.id} ~ /somi")
 
-        await interaction.response.send_message(f"Somi best grill\n{SOMI_BEST_GRILL_IMAGE}")
-
-        uses_update("command_uses", "somi")
+        await interaction.response.send_message(f"Somi best grill\n{self.client.SOMI_BEST_GRILL_IMAGE}")
 
 
 
-def setup(client):
+def setup(client: SomiBot):
     client.add_cog(Somi(client))

8046e97cb337204b21fafc23c62aeb4b63b5f876
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-12-18 00:41:48 +0100

    rewrote /lf np

--- cogs/lastfm/LastFmNowPlaying.py
+++ cogs/lastfm/LastFmNowPlaying.py
@@ -1,88 +1,90 @@
-###package#import###############################################################################
+####################################################################################################
 
 import nextcord
+import nextcord.ext.commands as nextcord_C
+import nextcord.ext.application_checks as nextcord_AC
 import requests
+import urllib.parse
 
-client = nextcord.ext.commands.Bot(intents=nextcord.Intents.all())
+####################################################################################################
 
-###self#imports###############################################################################
-
-from cogs._global_data.GlobalData import network
-from database.database_command_uses import uses_update
-from database.database_lastfm import lastfm_get_user_from_db
-from utilities.maincommands import checks
-from utilities.variables import LASTFM_ICON, LASTFM_COLOR
-from utilities.partial_commands import get_nick_else_name, embed_builder
+from lib.db_modules import LastFmDB
+from lib.modules import Checks, EmbedFunctions, Get
+from lib.utilities import SomiBot
 
 
 
-class LastFmNowPlaying(nextcord.ext.commands.Cog):
+class LastFmNowPlaying(nextcord_C.Cog):
 
     def __init__(self, client):
-        self.client = client
+        self.client: SomiBot = client
 
-    from utilities.maincommands import lastfm
+    from lib.utilities.main_commands import lastfm
 
-    ###lf#np###########################################################
+    ####################################################################################################
 
-    @lastfm.subcommand(name = "np", description = "shows what someone is listening to right now")
-    async def now_playing(self,
-                 interaction: nextcord.Interaction,
-                 *,
-                 member: nextcord.Member = nextcord.SlashOption(description="the user you want to be shown, what they're listening to", required=False)):
-        if not checks(interaction.guild, interaction.user):
-            return
+    @lastfm.subcommand(name = "nowplaying", description = "shows what someone is listening to right now", name_localizations = {country_tag:"np" for country_tag in nextcord.Locale})
+    @nextcord_AC.check(Checks().interaction_in_guild())
+    async def lastfm_now_playing(self,
+                                 interaction: nextcord.Interaction,
+                                 *,
+                                 member: nextcord.Member = nextcord.SlashOption(description="the user you want to be shown, what they're listening to", required=False)):
+        """This command displays what someone last listend to (and what they're listening to right now)"""
 
-        if member == None:
+        if not member:
             member = interaction.guild.get_member(interaction.user.id)
 
-        print(f"{interaction.user}: /lf np {member}")
+        self.client.Loggers.action_log(f"Guild: {interaction.guild.id} ~ Channel: {interaction.channel.id} ~ User: {interaction.user.id} ~ /lf np {member.id}")
 
-        lastfm_username = lastfm_get_user_from_db(member.id)
+        lastfm_username = LastFmDB().get_user(member.id)
+
+        if not lastfm_username:
+            await interaction.response.send_message(embed=EmbedFunctions().error(f"{member.mention} has not setup their LastFm account.\nTo setup a LastFm account use `/lf set`."), ephemeral=True)
+            return
 
         await interaction.response.defer(with_message = True)
 
-        if not lastfm_username:
-            await interaction.followup.send(f"{member.mention} has not setup their LastFm account.", ephemeral=True)
-            return
-
-        request_url = requests.get(f"http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&username={lastfm_username}&limit=1&api_key={network.api_key}&format=json")
+        request_url = requests.get(f"http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&username={lastfm_username}&limit=1&api_key={self.client.lf_network.api_key}&format=json")
 
         if not request_url.status_code == 200:
-            await interaction.followup.send("LastFm didn't respond correctly, try in a few minutes again!", ephemeral=True)
+            await interaction.followup.send(embed=EmbedFunctions().error("LastFm didn't respond correctly, try in a few minutes again!"))
             return
 
         np_user_data = request_url.json()
-        name = get_nick_else_name(member)
         cover_image = ""
         output = ""
 
         for track in np_user_data["recenttracks"]["track"]:
-            artist_name_for_url = track['artist']['#text'].replace(" ", "+")
-            album_name_for_url = track['album']['#text'].replace(" ", "+")
+            track_url = Get().markdown_safe(track['url'])
+            artist_name_for_url = urllib.parse.quote_plus(track['artist']['#text'])
+            album_name_for_url = urllib.parse.quote_plus(track['album']['#text'])
+
+            track_name = Get().markdown_safe(track['name'])
+            album_name = Get().markdown_safe(track['album']['#text'])
+            artist_name = Get().markdown_safe(track['artist']['#text'])
 
             if "date" in track:
                 timestamp = f"<t:{track['date']['uts']}:R>"
-                output += f"**[{track['name']}]({track['url']})** on [{track['album']['#text']}](https://www.last.fm/music/{artist_name_for_url}/{album_name_for_url}/)\nby [{track['artist']['#text']}](https://www.last.fm/music/{artist_name_for_url}/) - {timestamp}"
+                output += f"**[{track_name}]({track_url})** on [{album_name}](https://www.last.fm/music/{artist_name_for_url}/{album_name_for_url}/)\nby [{artist_name}](https://www.last.fm/music/{artist_name_for_url}/) - {timestamp}"
                 if cover_image == "":
                     cover_image = track['image'][3]['#text']
 
             else:
-                output += f"`Now Playing:`\n**[{track['name']}]({track['url']})** on [{track['album']['#text']}](https://www.last.fm/music/{artist_name_for_url}/{album_name_for_url}/)\nby [{track['artist']['#text']}](https://www.last.fm/music/{artist_name_for_url}/)\n\n`Previous:\n`"
+                output += f"`Now Playing:`\n**[{track_name}]({track_url})** on [{album_name}](https://www.last.fm/music/{artist_name_for_url}/{album_name_for_url}/)\nby [{artist_name}](https://www.last.fm/music/{artist_name_for_url}/)\n\n`Previous:\n`"
                 cover_image = track['image'][3]['#text']
 
-        embed = embed_builder(description = output,
-                              color = LASTFM_COLOR,
-                              thumbnail = cover_image,
-                              author = f"{name} is listening to:",
-                              author_icon = LASTFM_ICON,
-                              footer = "DEFAULT_KST_FOOTER")
+        embed = EmbedFunctions().builder(
+            color = self.client.LASTFM_COLOR,
+            thumbnail = cover_image,
+            author = f"{lastfm_username} is listening to:",
+            author_icon = self.client.LASTFM_ICON,
+            description = output,
+            footer = "DEFAULT_KST_FOOTER"
+        )
 
         await interaction.followup.send(embed=embed)
 
-        uses_update("command_uses", "lf np")
 
 
-
-def setup(client):
+def setup(client: SomiBot):
     client.add_cog(LastFmNowPlaying(client))

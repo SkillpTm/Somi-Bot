4dc9b38b76a834b5e97df24ed5991e40c74d9e3a
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-12-18 00:44:52 +0100

    rewrote close

--- cogs/mod/Close.py
+++ cogs/mod/Close.py
@@ -1,102 +1,129 @@
-###package#import###############################################################################
+####################################################################################################
 
 import nextcord
+import nextcord.ext.commands as nextcord_C
+import nextcord.ext.application_checks as nextcord_AC
 
-client = nextcord.ext.commands.Bot(intents=nextcord.Intents.all())
+####################################################################################################
 
-###self#imports###############################################################################
-
-from database.database_command_uses import uses_update
-from utilities.maincommands import checks
-from utilities.partial_commands import get_user_avatar, embed_builder
-from utilities.variables import AUDIT_LOG_ID, MODERATOR_ID, SOMMUNGCHI_ID
-
+from lib.db_modules import AuditLogChannelDB, DefaultRoleDB
+from lib.modules import Checks, EmbedFunctions
+from lib.utilities import SomiBot
 
 
-class Close(nextcord.ext.commands.Cog):
+
+class Close(nextcord_C.Cog):
 
     def __init__(self, client):
-        self.client = client
+        self.client: SomiBot = client
 
-    ###close###########################################################
+    ####################################################################################################
 
-    @nextcord.slash_command(name="close", description="[MOD] takes all regular users the sending message permission away")
-    @nextcord.ext.application_checks.has_permissions(manage_roles=True)
+    @nextcord.slash_command(name="close", description="takes all users through the default-role the send-messages permission away", default_member_permissions = nextcord.Permissions(manage_roles=True, manage_guild=True))
+    @nextcord_AC.check(Checks().interaction_in_guild())
     async def close(self,
                     interaction: nextcord.Interaction):
-        if not checks(interaction.guild, interaction.user):
+        """This command takes the send-messages permission from the default-role away, effectifly closing down the server."""
+
+        self.client.Loggers.action_log(f"Guild: {interaction.guild.id} ~ Channel: {interaction.channel.id} ~ User: {interaction.user.id} ~ /close")
+
+        await interaction.response.defer(ephemeral=True, with_message=True)
+
+        default_role_id = DefaultRoleDB().get(interaction.guild)
+
+        if not default_role_id:
+            await interaction.followup.send(embed=EmbedFunctions().error("You need to set a default-role, to be able to close the server.\nSet a default-role with `/config default-role`"), ephemeral=True)
             return
 
-        print(f"{interaction.user}: /close")
+        default_role = interaction.guild.get_role(default_role_id)
 
-        AUDIT_LOG = self.client.get_channel(AUDIT_LOG_ID)
-        SOMMUNGCHI = interaction.guild.get_role(SOMMUNGCHI_ID)
-        permissions = nextcord.Permissions()
+        permissions_to_check = (default_role.permissions.send_messages, default_role.permissions.send_messages_in_threads, default_role.permissions.add_reactions)
 
-        permissions.update(send_messages = False, send_messages_in_threads = False, add_reactions = False, view_channel = True, change_nickname = True, use_external_emojis = True, use_external_stickers = True, read_message_history = True, use_slash_commands = True, speak = True, stream = True, use_voice_activation = True, request_to_speak = True)
-        await SOMMUNGCHI.edit(permissions=permissions)
+        if not any(permission == True for permission in permissions_to_check):
+            await interaction.followup.send(embed=EmbedFunctions().error("The server is already closed.\n To re-open it use `/open`"), ephemeral=True)
+            return
 
-        await interaction.response.send_message("Closed the server sucessfully.", ephemeral=True)
+        await default_role.edit(permissions=nextcord.Permissions(permissions = default_role.permissions.value, send_messages = False, send_messages_in_threads = False, add_reactions = False))
 
-        member_avatar_url = get_user_avatar(interaction.user)
+        await interaction.followup.send(embed=EmbedFunctions().success("Closed the server sucessfully.\n To re-open it use `/open`"), ephemeral=True)
 
-        embed = embed_builder(color = nextcord.Color.dark_red(),
-                              author = "Mod Activity",
-                              author_icon = member_avatar_url,
-                              footer = "DEFAULT_KST_FOOTER",
 
-                              field_one_name = "/close:",
-                              field_one_value = f"{interaction.user.mention} closed the server!",
-                              field_one_inline = True)
+        audit_log_id = AuditLogChannelDB().get(interaction.guild)
 
-        await AUDIT_LOG.send(embed=embed, content = f"<@&{MODERATOR_ID}>")
+        if not audit_log_id:
+            return
 
-        uses_update("mod_command_uses", "close")
+        embed = EmbedFunctions().builder(
+            color = nextcord.Color.dark_red(),
+            author = "Mod Activity",
+            author_icon = interaction.user.display_avatar,
+            footer = "DEFAULT_KST_FOOTER",
+            fields = [
+                [
+                    "/close:",
+                    f"{interaction.user.mention} closed the server!",
+                    False
+                ]
+            ]
+        )
 
-    @close.error
-    async def close_error(self, interaction: nextcord.Interaction, error):
-        await interaction.response.send_message(f"Only <@&{MODERATOR_ID}> can use this command.", ephemeral=True)
+        audit_log_channel = interaction.guild.get_channel(audit_log_id)
+        await audit_log_channel.send(embed=embed)
 
-    ###open###########################################################
+    ####################################################################################################
 
-    @nextcord.slash_command(name="open", description="[MOD] gives all regular users the sending message permission back")
-    @nextcord.ext.application_checks.has_permissions(manage_roles=True)
+    @nextcord.slash_command(name="unclose", description="gives users through the default-role the send-messages permission back", default_member_permissions = nextcord.Permissions(manage_roles=True, manage_guild=True), name_localizations = {country_tag:"open" for country_tag in nextcord.Locale})
+    @nextcord_AC.check(Checks().interaction_in_guild())
     async def open(self,
                    interaction: nextcord.Interaction):
-        if not checks(interaction.guild, interaction.user):
+        """This command gives the send-messages permission back to the default-role, effectifly opening the server back up."""
+
+        self.client.Loggers.action_log(f"Guild: {interaction.guild.id} ~ Channel: {interaction.channel.id} ~ User: {interaction.user.id} ~ /open")
+
+        await interaction.response.defer(ephemeral=True, with_message=True)
+
+        default_role_id = DefaultRoleDB().get(interaction.guild)
+
+        if not default_role_id:
+            await interaction.followup.send(embed=EmbedFunctions().error("You need to set a default-role, to be able to open the server.\nSet a default-role with `/config default-role`"), ephemeral=True)
             return
 
-        print(f"{interaction.user}: /open")
+        default_role = interaction.guild.get_role(default_role_id)
 
-        AUDIT_LOG = self.client.get_channel(AUDIT_LOG_ID)
-        SOMMUNGCHI = interaction.guild.get_role(SOMMUNGCHI_ID)
-        permissions = nextcord.Permissions()
+        permissions_to_check = (default_role.permissions.send_messages, default_role.permissions.send_messages_in_threads, default_role.permissions.add_reactions)
 
-        permissions.update(send_messages = True, send_messages_in_threads = True, add_reactions = True, view_channel = True, change_nickname = True, use_external_emojis = True, use_external_stickers = True, read_message_history = True, use_slash_commands = True, speak = True, stream = True, use_voice_activation = True,    request_to_speak = True)
-        await SOMMUNGCHI.edit(permissions=permissions)
+        if not any(permission == False for permission in permissions_to_check):
+            await interaction.followup.send(embed=EmbedFunctions().error("The server is already open.\n To close it use `/close`"), ephemeral=True)
+            return
 
-        await interaction.response.send_message("Re-opened the server sucessfully.", ephemeral=True)
+        await default_role.edit(permissions=nextcord.Permissions(permissions = default_role.permissions.value, send_messages = True, send_messages_in_threads = True, add_reactions = True))
 
-        member_avatar_url = get_user_avatar(interaction.user)
-
-        embed = embed_builder(color = nextcord.Color.dark_red(),
-                              author = "Mod Activity",
-                              author_icon = member_avatar_url,
-                              footer = "DEFAULT_KST_FOOTER",
-
-                              field_one_name = "/open:",
-                              field_one_value = f"{interaction.user.mention} re-opened the server!",
-                              field_one_inline = True)
-
-        await AUDIT_LOG.send(embed=embed)
-
-        uses_update("mod_command_uses", "open")
-
-    @open.error
-    async def open_error(self, interaction: nextcord.Interaction, error):
-        await interaction.response.send_message(f"Only <@&{MODERATOR_ID}> can use this command.", ephemeral=True)
+        await interaction.followup.send(embed=EmbedFunctions().success("Re-opened the server sucessfully."), ephemeral=True)
 
 
+        audit_log_id = AuditLogChannelDB().get(interaction.guild)
 
-def setup(client):
+        if not audit_log_id:
+            return
+
+        embed = EmbedFunctions().builder(
+            color = nextcord.Color.dark_red(),
+            author = "Mod Activity",
+            author_icon = interaction.user.display_avatar,
+            footer = "DEFAULT_KST_FOOTER",
+            fields = [
+                [
+                    "/open:",
+                    f"{interaction.user.mention} re-opened the server!",
+                    False
+                ]
+            ]
+        )
+
+        audit_log_channel = interaction.guild.get_channel(audit_log_id)
+        await audit_log_channel.send(embed=embed)
+
+
+
+def setup(client: SomiBot):
     client.add_cog(Close(client))

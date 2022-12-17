7c18f51cae06a7f87b40bd2263464914c2846da8
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-12-18 00:41:40 +0100

    rewrote /lf artist

--- cogs/lastfm/LastFmArtist.py
+++ cogs/lastfm/LastFmArtist.py
@@ -1,90 +1,112 @@
-###package#import###############################################################################
+####################################################################################################
 
+from bs4 import BeautifulSoup
 import nextcord
+import nextcord.ext.commands as nextcord_C
+import nextcord.ext.application_checks as nextcord_AC
 import requests
 import re
-from bs4 import BeautifulSoup
+import urllib.parse
 
-client = nextcord.ext.commands.Bot(intents=nextcord.Intents.all())
+####################################################################################################
 
-###self#imports###############################################################################
-
-from cogs._global_data.GlobalData import network
-from database.database_command_uses import uses_update
-from database.database_lastfm import lastfm_get_user_from_db
-from utilities.maincommands import checks
-from utilities.variables import LASTFM_ICON, LASTFM_COLOR
-from utilities.partial_commands import get_nick_else_name, embed_builder
+from lib.db_modules import LastFmDB
+from lib.modules import Checks, EmbedFunctions, Get
+from lib.utilities import LASTFM_TIMEFRAMES_ARTIST, LASTFM_TIMEFRAMES_ARTIST_TEXT, LASTFM_COOKIES, LASTFM_HEADERS, SomiBot
 
 
 
-class LastFmArtist(nextcord.ext.commands.Cog):
+class LastFmArtist(nextcord_C.Cog):
 
     def __init__(self, client):
-        self.client = client
+        self.client: SomiBot = client
 
-    from utilities.maincommands import lastfm
+    from lib.utilities.main_commands import lastfm
 
-    ###lf#artist###########################################################
+    ####################################################################################################
 
     @lastfm.subcommand(name = "artist", description = "shows you your LastFm stats for any artist")
-    async def artist(self,
-                     interaction: nextcord.Interaction,
-                     *,
-                     artist: str = nextcord.SlashOption(description="the artist you want to see your stats for", required=False, min_length=1, max_length=100),
-                     member: nextcord.Member = nextcord.SlashOption(description="the user you want to be shown, what they're listening to", required=False),
-                     timeframe: str = nextcord.SlashOption(description="the timeframe you want the top albums for", required=False, choices = {"Past Week": "7day", "Past Month": "1month", "Past Quarter": "3month", "Past Half a Year": "6month", "Past Year": "12month", "All Time": "overall"})):
-        if not checks(interaction.guild, interaction.user):
-            return
+    @nextcord_AC.check(Checks().interaction_in_guild())
+    async def lastfm_artist(self,
+                            interaction: nextcord.Interaction,
+                            *,
+                            artist: str = nextcord.SlashOption(description="the artist you want to see your stats for", required=False, min_length=2, max_length=100),
+                            member: nextcord.Member = nextcord.SlashOption(description="the user you want to be shown, what they're listening to", required=False),
+                            timeframe: str = nextcord.SlashOption(description="the timeframe you want the top albums for", required=False, choices=LASTFM_TIMEFRAMES_ARTIST)):
+        """This command webscrapes the data of a user from LastFm to get their top tracks and top albums for a certain artist"""
 
-        if member == None:
+        if not member:
             member = interaction.guild.get_member(interaction.user.id)
 
-        if timeframe == None:
+        if not timeframe:
             timeframe = "overall"
 
-        lastfm_username = lastfm_get_user_from_db(member.id)
+        lastfm_username = LastFmDB().get_user(member.id)
 
         if not lastfm_username:
-            await interaction.response.send_message(f"{member.mention} has not setup their LastFm account.", ephemeral=True)
+            await interaction.response.send_message(embed=EmbedFunctions().error(f"{member.mention} has not setup their LastFm account.\nTo setup a LastFm account use `/lf set`."), ephemeral=True)
             return
 
         await interaction.response.defer(with_message = True)
         
-        if artist == None:
-            request_url = requests.get(f"http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&username={lastfm_username}&limit=1&api_key={network.api_key}&format=json")
+        if not artist:
+            request_url = requests.get(f"http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&username={lastfm_username}&limit=1&api_key={self.client.lf_network.api_key}&format=json")
 
             if not request_url.status_code == 200:
-                await interaction.followup.send("LastFm didn't respond correctly, try in a few minutes again!")
+                await interaction.followup.send(embed=EmbedFunctions().error("LastFm didn't respond correctly, try in a few minutes again!"))
                 return
 
             np_user_data = request_url.json()
             artist =  np_user_data["recenttracks"]["track"][0]['artist']['#text']
 
-        print(f"{interaction.user}: /lf artist {artist} {lastfm_username} {timeframe}")
+        self.client.Loggers.action_log(f"Guild: {interaction.guild.id} ~ Channel: {interaction.channel.id} ~ User: {interaction.user.id} ~ /lf artist {artist} {member.id} {timeframe}")
 
-        # WEBSCRAPING
 
-        artist_for_url = artist.replace(" ", "+")
-        page = requests.get(f"https://www.last.fm/user/{lastfm_username}/library/music/{artist_for_url}?date_preset={timeframe}")
+        artist_for_url = urllib.parse.quote_plus(artist)
+        page = requests.get(f"https://www.last.fm/user/{lastfm_username}/library/music/{artist_for_url}?date_preset={timeframe}", cookies=LASTFM_COOKIES, headers=LASTFM_HEADERS)
 
         if not page.status_code == 200:
-            await interaction.followup.send(f"The artist `{artist}` couldn't be found on LastFm.")
+            await interaction.followup.send(embed=EmbedFunctions().error(f"The artist `{artist}` couldn't be found on LastFm."))
             return
 
         soup = BeautifulSoup(page.content, "html.parser")
 
         if "didn't scrobble any albums by this artist during the selected date range. Try expanding the date range or view scrobbles for " in str(soup.text):
-            await interaction.followup.send(f"`{lastfm_username}` hasn't listened to the artist `{artist}` in the timeframe: `{timeframe}`")
+            await interaction.followup.send(embed=EmbedFunctions().error(f"{member.mention} hasn't listened to the artist `{artist}` in the timeframe: `{LASTFM_TIMEFRAMES_ARTIST_TEXT[timeframe]}`"))
             return
 
-        artist_name = soup.find("h2", class_="library-header-title") # Artist name
+        artist_name, cover_image, artist_stats, album_output, track_output = self.webscrape_artist_page(soup, artist_for_url)
+
+        embed = EmbedFunctions().builder(
+            color = self.client.LASTFM_COLOR,
+            thumbnail = cover_image,
+            author = f"{lastfm_username} stats for {artist_name.text} {LASTFM_TIMEFRAMES_ARTIST_TEXT[timeframe]}",
+            author_url = f"https://www.last.fm/user/{lastfm_username}/library/music/{artist_for_url}?date_preset={timeframe}",
+            author_icon = self.client.LASTFM_ICON,
+            description = f"`{lastfm_username}` has __**{artist_stats[0]}**__ scrobbles for this artist.\n" +
+                          f"`{lastfm_username}` has played **{artist_stats[2]}** tracks on **{artist_stats[1]}** of their albums.\n\n" +
+                          f"**Top Albums**\n" +
+                          f"{album_output}\n" +
+                          f"**Top Tracks**\n" +
+                          f"{track_output}\n",
+            footer = "DEFAULT_KST_FOOTER"
+        )
+
+        await interaction.followup.send(embed=embed)
+
+    ####################################################################################################
+
+    @staticmethod
+    def webscrape_artist_page(soup, artist_for_url):
+        """This function webscrapes the artist page of a given user"""
+
+        artist_name: str = soup.find("h2", class_="library-header-title") # Artist name
 
         results_image = soup.find("span", class_="avatar library-header-image")
-        cover_image = results_image.img["src"] # Artist iamge
+        cover_image: str = results_image.img["src"] # Artist iamge
 
         results_stats = soup.find_all("p", class_="metadata-display")
-        artist_stats = [result.text for result in results_stats] # (Scrobbles, Albums, Tracks) amount
+        artist_stats = [stat.text for stat in results_stats] # (Scrobbles, Albums, Tracks) amount
 
         results_albums_and_tracks = soup.find_all("tbody")
         album_output = "" # index_number, album with url - scrobble amount
@@ -111,9 +133,10 @@ class LastFmArtist(nextcord.ext.commands.Cog):
 
                     # album/track with url    
                     if 'chartlist-name' in str(td): # unique class name of the td needed
-                        name = re.sub('\n', '', td.text) # can be both album and track
+                        name = Get().markdown_safe(re.sub('\n', '', td.text)) # can be both album and track
+
                         name_for_url = re.sub('\n', '', td.text)
-                        name_for_url = re.sub(' ', '+', name_for_url)
+                        name_for_url = urllib.parse.quote_plus(name_for_url)
 
                         if i == 1:
                             album_output += f"[{name}](https://www.last.fm/music/{artist_for_url}/{name_for_url}/) " # URL and album name
@@ -128,35 +151,22 @@ class LastFmArtist(nextcord.ext.commands.Cog):
                                     if "chartlist-count-bar-value" in str(span2): # unique class name of the span needed
                                         scorbble_amount = re.sub('\n', '', span2.text)
                                         scorbble_amount = re.sub(' ', '', scorbble_amount)
-                                        scorbble_amount = scorbble_amount.replace('scrobbles', ' plays') # has to be done after re.sub, to keep that space before plays
+                                        SCROBBLE_REPLACMENTS = {
+                                            "scrobbles": "plays",
+                                            "scrobble": "play"
+                                        }
+
+                                        for key, value in SCROBBLE_REPLACMENTS.items():
+                                            scorbble_amount = scorbble_amount.replace(key, f" {value}") # has to be done after re.sub, to keep that space before plays
 
                                         if i == 1:
                                             album_output += f"- *({scorbble_amount})*\n" # Scrobble amount for album/track
                                         elif i == 2:
                                             track_output += f"- *({scorbble_amount})*\n"# Scrobble amount for album/track
 
-        name = get_nick_else_name(member)
-        authot_text_dict = {"7day": "of the past week:", "1month": "of the past month:", "3month": "of the past quarter:", "6month": "of the past half a year:", "12month": "of the past year:", "overall": "of all time:"}
-
-        embed = embed_builder(description = f"""You have __**{artist_stats[0]}**__ scrobbles for this artist.
-        You have played **{artist_stats[2]}** tracks on **{artist_stats[1]}** of their albums.
-        
-        **Top Albums**
-        {album_output}
-        **Top Tracks**
-        {track_output}""",
-                              color = LASTFM_COLOR,
-                              thumbnail = cover_image,
-                              author = f"{name} stats for {artist_name.text} {authot_text_dict[timeframe]}",
-                              author_url = f"https://www.last.fm/user/{lastfm_username}/library/music/{artist_for_url}?date_preset={timeframe}",
-                              author_icon = LASTFM_ICON,
-                              footer = "DEFAULT_KST_FOOTER")
-
-        await interaction.followup.send(embed=embed)
-
-        uses_update("command_uses", "lf artist")
+        return artist_name, cover_image, artist_stats, album_output, track_output
 
 
 
-def setup(client):
+def setup(client: SomiBot):
     client.add_cog(LastFmArtist(client))

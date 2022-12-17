0116ac5be06d79223f2749881deab603c2c819db
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-12-18 00:42:50 +0100

    rewrote xp gain

--- cogs/level/LevelsGainXp.py
+++ cogs/level/LevelsGainXp.py
@@ -1,58 +1,60 @@
-###package#import###############################################################################
+####################################################################################################
 
 import nextcord
+import nextcord.ext.commands as nextcord_C
+import re
 import time
 
-client = nextcord.ext.commands.Bot(intents=nextcord.Intents.all())
+####################################################################################################
 
-###self#imports###############################################################################
-
-from database.database_levels import check_level_for_server_and_user, get_user_cooldown, increase_user_xp, get_user_level
-from database.database_levels_ignore import make_ignore_channel_table, ignore_channels_list
-from database.database_levelroles import check_levelroles_for_server_role_and_level
-from utilities.maincommands import checks
-from utilities.partial_commands import level_roles_apply
+from lib.db_modules import LevelsDB, LevelIgnoreChannelsDB
+from lib.modules import Checks, LevelRoles
+from lib.utilities import SomiBot
 
 
 
-class LevelsGainXp(nextcord.ext.commands.Cog):
+class LevelsGainXp(nextcord_C.Cog):
 
     def __init__(self, client):
-        self.client = client
+        self.client: SomiBot = client
 
-    ###gain#xp###########################################################
+    ####################################################################################################
 
-    @nextcord.ext.commands.Cog.listener()
+    @nextcord_C.Cog.listener()
     async def on_message(self,
                          message: nextcord.Message):
-        if not checks(self.client, message.guild, message.author):
+        """This command gives xp to a user on the server the message was send, if the message:
+            1. is longer than 10 characters
+            2. was not send in a levels ignore channel
+            3. was send after the user's cooldown went off
+            
+            afterwards the command will try to apply levelroles"""                 
+
+        if not Checks.message_in_guild(self.client, message):
             return
 
-        if not len(message.content) > 10:
+        if not len(re.sub('<[^ ]+?>', "",message.content)) > 10:
             return
 
-        make_ignore_channel_table(message.guild.id)
-
-        ignore_channel_ids = ignore_channels_list(message.guild.id)
+        ignore_channel_ids = LevelIgnoreChannelsDB().channels_list(message.guild)
 
         if message.channel.id in ignore_channel_ids:
             return
 
-        check_level_for_server_and_user(message.guild.id, message.author.id)
+        LevelsDB().insert_user(message.guild.id, message.author.id)
 
-        cooldown_time = get_user_cooldown(message.guild.id, message.author.id)
+        cooldown_time = LevelsDB().get_user_cooldown(message.guild.id, message.author.id)
 
         if cooldown_time >= int(time.time()):
             return
 
-        increase_user_xp(message.guild.id, message.author.id)
+        LevelsDB().increase_user_xp(message.guild.id, message.author.id)
 
-        user_level, total_xp = get_user_level(message.guild.id, message.author.id)
+        user_level, xp_until_next_level =  LevelsDB().get_user_level(message.guild.id, message.author.id)
 
-        check_levelroles_for_server_role_and_level(message.guild.id)
-
-        await level_roles_apply(message.guild, [[message.author.id, user_level]])
+        await LevelRoles().apply(message.guild, [[message.author.id, user_level]])
 
 
-def setup(client):
+
+def setup(client: SomiBot):
     client.add_cog(LevelsGainXp(client))

9091a7135873b0a302990e0861b5f4f53de66c86
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-12-18 00:48:41 +0100

    added init files for easy imports

---
+++ lib/modules/__init__.py
@@ -0,0 +1,5 @@
+from lib.modules.Checks import Checks
+from lib.modules.Create import Create
+from lib.modules.EmbedFunctions import EmbedFunctions
+from lib.modules.Get import Get
+from lib.modules.LevelRoles import LevelRoles

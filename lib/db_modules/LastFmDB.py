6084a1d91d9c206e58244516ed259132f20d81d3
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-12-18 00:49:16 +0100

    rewrote lastfm db

--- lib/db_modules/LastFmDB.py
+++ lib/db_modules/LastFmDB.py
@@ -1,52 +1,88 @@
-import sqlite3
+####################################################################################################
+
 import os
+import sqlite3
 
-###lastfm#check###########################################################
+####################################################################################################
 
-def lastfm_get_user_from_db(user_id):
-    database_path = os.path.join(os.path.dirname(__file__), '../storage/db/lastfm.db')
-    conn = sqlite3.connect(database_path)
 
-    c = conn.cursor()
 
-    c.execute(f"SELECT lastfm_username FROM lastfmUsers WHERE discord_user_id = {str(user_id)}")
+class LastFmDB():
 
-    tuple_lastfm_username = c.fetchone()
+    def __init__(self) -> None:
+        self.database_path = os.path.join(os.path.dirname(__file__), '../../storage/db/lastfm.db')
 
-    if tuple_lastfm_username == None:
+    ####################################################################################################
+
+    def create_table(self) -> None:
+        """Creates a table, if there isn't one already"""
+
+        conn = sqlite3.connect(self.database_path)
+        c = conn.cursor()
+
+        c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='lastfmUsers'")
+
+        if not c.fetchone():
+            c.execute("""CREATE TABLE lastfmUsers (discord_user_id text,
+                                                   lastfm_username text)""")
+
+            conn.commit()
+
         conn.close()
-        return False
 
-    for username in tuple_lastfm_username:
-        lastfm_username = username
+    ####################################################################################################
 
-    conn.close()
-    return lastfm_username
+    def get_user(self,
+                 user_id: int) -> str:
+        """This function returna the lf username of a discord user, if they have set one"""
 
-###lastfm#set###########################################################
+        self.create_table()
 
-def lastfm_set_user(user_id, lastfm_username):
-    database_path = os.path.join(os.path.dirname(__file__), '../storage/db/lastfm.db')
-    conn = sqlite3.connect(database_path)
+        conn = sqlite3.connect(self.database_path)
+        c = conn.cursor()
 
-    c = conn.cursor()
+        c.execute(f"SELECT lastfm_username FROM lastfmUsers WHERE discord_user_id = '{user_id}'")
 
-    c.execute(f"INSERT INTO lastfmUsers VALUES ('{str(user_id)}', '{lastfm_username}')")
+        lf_user = c.fetchone()
 
-    conn.commit()
+        if not lf_user:
+            conn.close()
+            return None
 
-    conn.close()
+        conn.close()
+        return lf_user[0]
 
-###lastfm#reset###########################################################
+    ####################################################################################################
 
-def lastfm_reset_user(user_id):
-    database_path = os.path.join(os.path.dirname(__file__), '../storage/db/lastfm.db')
-    conn = sqlite3.connect(database_path)
+    def set_user(self,
+                 user_id: int,
+                 lastfm_username: str) -> None:
+        """This function connects a discord and lastfm user in the db"""
 
-    c = conn.cursor()
+        self.create_table()
 
-    c.execute(f"DELETE FROM lastfmUsers WHERE discord_user_id = '{str(user_id)}'")
+        conn = sqlite3.connect(self.database_path)
+        c = conn.cursor()
 
-    conn.commit()
+        c.execute(f"INSERT INTO lastfmUsers VALUES ('{user_id}', '{lastfm_username}')")
 
-    conn.close()
+        conn.commit()
+
+        conn.close()
+
+    ####################################################################################################
+
+    def reset_user(self,
+                   user_id: int) -> None:
+        """This function removes a user's info from the db"""
+
+        self.create_table()
+
+        conn = sqlite3.connect(self.database_path)
+        c = conn.cursor()
+
+        c.execute(f"DELETE FROM lastfmUsers WHERE discord_user_id = '{str(user_id)}'")
+
+        conn.commit()
+
+        conn.close()

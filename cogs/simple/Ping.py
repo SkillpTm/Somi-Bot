ce2aeeb27fc192518f54faa487f3b0565e03293a
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-12-18 00:47:53 +0100

    rewrote /ping

--- cogs/simple/Ping.py
+++ cogs/simple/Ping.py
@@ -1,48 +1,48 @@
-###package#import###############################################################################
+####################################################################################################
 
 import nextcord
+import nextcord.ext.commands as nextcord_C
+import nextcord.ext.application_checks as nextcord_AC
 import time
 
-client = nextcord.ext.commands.Bot(intents=nextcord.Intents.all())
+####################################################################################################
 
-###self#imports###############################################################################
-
-from cogs._global_data.GlobalData import start_time
-from database.database_command_uses import uses_update
-from utilities.maincommands import checks
+from lib.modules import Checks, Get
+from lib.utilities import SomiBot
 
 
 
-class Ping(nextcord.ext.commands.Cog):
+class Ping(nextcord_C.Cog):
 
     def __init__(self, client):
-        self.client = client
+        self.client: SomiBot = client
 
-    ###ping###########################################################
+    ####################################################################################################
 
     @nextcord.slash_command(name = "ping", description = "shows the bot's ping to discord")
+    @nextcord_AC.check(Checks().interaction_in_guild())
     async def ping(self,
                    interaction: nextcord.Interaction):
+        """This command shows the ping and some general stats about the bot"""
+
         start = time.time()
-        if not checks(interaction.guild, interaction.user):
-            return
 
-        print(f"{interaction.user}: /ping")
+        self.client.Loggers.action_log(f"Guild: {interaction.guild.id} ~ Channel: {interaction.channel.id} ~ User: {interaction.user.id} ~ /ping")
 
-        await interaction.response.send_message(f"""ping = `{self.client.latency * 1000:,.0f}ms`
-        uptime = <t:{start_time}:R>
-        Visible Users: `{len(self.client.users)}`""")
+        visible_users = Get().visible_users(self.client)
+
+        await interaction.response.send_message(content = f"DWSP latency (ping) = `{self.client.latency * 1000:,.0f}ms`" + 
+                                                          f"\nUptime = <t:{self.client.start_time}:R>" + 
+                                                          f"\nVisible Users: `{visible_users}`")
 
         end = time.time()
         
-        await interaction.edit_original_message(f"""DWSP latency (ping) = `{self.client.latency * 1000:,.0f}ms`
-        Response time: `{(end-start) * 1000 :,.0f}ms`
-        Uptime = <t:{start_time}:R>
-        Visible Users: `{len(self.client.users)}`""")
-
-        uses_update("command_uses", "ping")
+        await interaction.edit_original_message(content = f"DWSP latency (ping) = `{self.client.latency * 1000:,.0f}ms`" + 
+                                                          f"\nResponse time: `{(end-start) * 1000 :,.0f}ms`" +
+                                                          f"\nUptime = <t:{self.client.start_time}:R>" +
+                                                          f"\nVisible Users: `{visible_users}`")
 
 
 
-def setup(client):
+def setup(client: SomiBot):
     client.add_cog(Ping(client))

2e35f5ee08f4be08cc18d24b294e40149f988c50
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-12-18 00:50:44 +0100

    added create module

---
+++ lib/modules/Create.py
@@ -0,0 +1,38 @@
+####################################################################################################
+
+import csv
+import nextcord
+
+####################################################################################################
+
+
+
+class Create():
+
+    def __init__(self) -> None:
+        pass
+
+    ####################################################################################################
+    
+    @staticmethod
+    def bulk_messages_csv(messages: list[nextcord.Message]) -> None:
+        """This function creates a CSV file, which saves the author id/name, the time and the content of messages"""
+
+        messages.reverse()
+        fieldnames = ["Author ID", "Author Name", "Send at", "Content"]
+
+        with open(f"./storage/temp/bulk_messages_{messages[0].guild.id}_{messages[0].channel.id}_{len(messages)}.csv", "w") as file:
+            writer = csv.DictWriter(file, fieldnames=fieldnames)
+            writer.writeheader()
+
+            for message in messages:
+                message_sent_time = message.created_at.strftime("%Y/%m/%d %H:%M:%S")
+
+                writer.writerow(
+                    {
+                        "Author ID": message.author.id,
+                        "Author Name": message.author.name,
+                        "Send at": message_sent_time,
+                        "Content": message.content
+                    }
+                )

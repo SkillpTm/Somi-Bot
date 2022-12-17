ec952952d237b830147a946b6708c0db65ec9d5f
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-12-18 00:44:01 +0100

    rewrote join log

--- cogs/logs/JoinLog.py
+++ cogs/logs/JoinLog.py
@@ -1,70 +1,88 @@
-###package#import###############################################################################
+####################################################################################################
 
 import nextcord
+import nextcord.ext.commands as nextcord_C
 import time
 
-client = nextcord.ext.commands.Bot(intents=nextcord.Intents.all())
+####################################################################################################
 
-###self#imports###############################################################################
-
-from database.database_command_uses import uses_update
-from utilities.maincommands import checks
-from utilities.variables import AUDIT_LOG_ID, SOMMUNGCHI_ID, WELCOME_CHANNEL_ID, WELCOME_GIF, SERVER_ID
-from utilities.partial_commands import get_user_avatar, embed_builder
+from lib.db_modules import AuditLogChannelDB, DefaultRoleDB, CommandUsesDB
+from lib.modules import EmbedFunctions
+from lib.utilities import SomiBot
 
 
 
-class JoinLog(nextcord.ext.commands.Cog):
+class JoinLog(nextcord_C.Cog):
 
     def __init__(self, client):
-        self.client = client
+        self.client: SomiBot = client
 
-    ###welcome#message###########################################################
+    ####################################################################################################
     
-    @nextcord.ext.commands.Cog.listener()
+    @nextcord_C.Cog.listener()
     async def on_member_join(self,
-                             member):
-        if not checks(member.guild, member.user):
+                             member: nextcord.Member):
+        """
+        This function will:
+        - Send a welcome message to Somicord
+        - give the new member a default role, if set
+        - create a join-log message, if teh guild has a join-log
+        """
+
+        self.client.Loggers.action_log(f"Guild: {member.guild.id} ~ User: {member.id} ~ join_log()")
+
+        ####################################################################################################
+
+        if member.guild.id == self.client.SOMICORD_ID:
+            SOMICORD_WELCOME_CHANNEL = self.client.get_channel(self.client.SOMICORD_WELCOME_CHANNEL_ID)
+            await SOMICORD_WELCOME_CHANNEL.send(f"Hey {member.mention}, welcome to Jeon Somi! What you waiting for - start chatting.\n{self.client.SOMICORD_WELCOME_GIF}")
+
+        ####################################################################################################
+
+        default_role_id = DefaultRoleDB().get(member.guild)
+
+        if default_role_id:
+            await member.add_roles(member.guild.get_role(default_role_id))
+
+        ####################################################################################################
+
+        audit_log_id = AuditLogChannelDB().get(member.guild)
+
+        if not audit_log_id:
             return
-        if member.guild.id != SERVER_ID:
-            return
 
-        print(f"join_log() {member}")
+        embed = EmbedFunctions().builder(
+            color = nextcord.Color.green(),
+            thumbnail = member.display_avatar,
+            title = f"New Member Joined: `{member}`",
+            footer = "DEFAULT_KST_FOOTER",
+            fields = [
+                [
+                    "ID:",
+                    member.id,
+                    False
+                ],
 
-        DEFAULT_ROLE = nextcord.utils.get(member.guild.roles, id=SOMMUNGCHI_ID)
-        WELCOME_CHANNEL = self.client.get_channel(WELCOME_CHANNEL_ID)
+                [
+                    "Name:",
+                    member.mention,
+                    True
+                ],
 
-        await member.add_roles(DEFAULT_ROLE)
-        await WELCOME_CHANNEL.send(f"Hey {member.mention}, welcome to Jeon Somi! What you waiting for - start chatting.\n{WELCOME_GIF}")
+                [
+                    "Created at:",
+                    f"<t:{int(time.mktime(member.created_at.timetuple()))}>",
+                    True
+                ]
+            ]
+        )
 
-    ###join#log###########################################################
+        audit_log_channel = member.guild.get_channel(audit_log_id)
+        await audit_log_channel.send(embed=embed)
 
-        created_time = int(time.mktime(member.created_at.timetuple()))
-        AUDIT_LOG = self.client.get_channel(AUDIT_LOG_ID)
-        member_avatar_url = get_user_avatar(member)
-
-        embed = embed_builder(title = f"New Member Joined: `{member}`",
-                              color = nextcord.Color.green(),
-                              thumbnail = member_avatar_url,
-                              footer = "DEFAULT_KST_FOOTER",
-
-                              field_one_name = "ID:",
-                              field_one_value = member.id,
-                              field_one_inline = False,
-
-                              field_two_name = "Name:",
-                              field_two_value = member.mention,
-                              field_two_inline = True,
-                              
-                              field_three_name = "Created at:",
-                              field_three_value = f"<t:{created_time}>",
-                              field_three_inline = True)
-
-        await AUDIT_LOG.send(embed=embed)
-
-        uses_update("log_activations", "join log")
+        CommandUsesDB().uses_update("log_activations", "join log")
 
 
 
-def setup(client):
+def setup(client: SomiBot):
     client.add_cog(JoinLog(client))

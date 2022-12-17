1ef159f86b80c2334972f76bc8de8e1653c1e30a
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-12-18 00:49:27 +0100

    rewrote db

--- lib/db_modules/RedditDB.py
+++ lib/db_modules/RedditDB.py
@@ -1,35 +1,67 @@
-import sqlite3
+####################################################################################################
+
 import os
+import sqlite3
 
-###get#relevant#history#ids###########################################################
+####################################################################################################
 
-def get_history_ids():
-    database_path = os.path.join(os.path.dirname(__file__), '../storage/db/subreddit_history.db')
-    conn = sqlite3.connect(database_path)
+class RedditDB():
 
-    c = conn.cursor()
+    def __init__(self) -> None:
+        self.database_path = os.path.join(os.path.dirname(__file__), '../../storage/db/subreddit_history.db')
 
-    c.execute(f"SELECT id FROM somi ORDER BY id DESC LIMIT 201")
-    tuple_history_ids = c.fetchall()
-    history_ids = []
+    ####################################################################################################
 
-    for id in tuple_history_ids:
-        history_ids.append(id[0])
+    def create_table(self,
+                     sub_reddit: str) -> None:
+        """Creates a table, if there isn't one already"""
 
-    conn.close()
+        conn = sqlite3.connect(self.database_path)
+        c = conn.cursor()
 
-    return history_ids
+        c.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{sub_reddit}'")
 
-###add#post#id#to#db###########################################################
+        if not c.fetchone():
+            c.execute(f"CREATE TABLE {sub_reddit} (id text)")
 
-def add_new_id_to_database(new_id):
-    database_path = os.path.join(os.path.dirname(__file__), '../storage/db/subreddit_history.db')
-    conn = sqlite3.connect(database_path)
+            conn.commit()
 
-    c = conn.cursor()
+        conn.close()
 
-    c.execute(f"INSERT INTO somi VALUES ('{new_id}')")
+    ####################################################################################################
 
-    conn.commit()
+    def get_history_ids(self,
+                        sub_reddit: str) -> list[str]:
+        """This function gets the recent history of the named subreddit"""
 
-    conn.close()
+        self.create_table(sub_reddit)
+
+        conn = sqlite3.connect(self.database_path)
+        c = conn.cursor()
+
+        c.execute(f"SELECT id FROM {sub_reddit} ORDER BY id DESC LIMIT 1000")
+        tuple_history_ids = c.fetchall()
+
+        history_ids = [id[0] for id in tuple_history_ids]
+
+        conn.close()
+
+        return history_ids
+
+    ####################################################################################################
+
+    def add_new_id(self,
+                   sub_reddit: str,
+                   new_id: str) -> None:
+        """This function adds a new id into the table of it's subreddit"""
+
+        self.create_table(sub_reddit)
+
+        conn = sqlite3.connect(self.database_path)
+        c = conn.cursor()
+
+        c.execute(f"INSERT INTO {sub_reddit} VALUES ('{new_id}')")
+
+        conn.commit()
+
+        conn.close()

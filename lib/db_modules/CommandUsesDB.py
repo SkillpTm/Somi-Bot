101eec22668451b80a198120ee0b8150cab8badc
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2022-12-18 00:48:55 +0100

    rewrote command uses db

--- lib/db_modules/CommandUsesDB.py
+++ lib/db_modules/CommandUsesDB.py
@@ -1,32 +1,58 @@
-import sqlite3
+####################################################################################################
+
 import os
+import sqlite3
 
-###uses#update###########################################################
+####################################################################################################
 
-def uses_update(table, column_name):
-    database_path = os.path.join(os.path.dirname(__file__), '../storage/db/command_uses.db')
-    conn = sqlite3.connect(database_path)
+class CommandUsesDB():
 
-    c = conn.cursor()
+    def __init__(self) -> None:
+        self.database_path = os.path.join(os.path.dirname(__file__), '../../storage/db/command_uses.db')
 
-    c.execute(f"SELECT name FROM {table}")
-    tuple_all_column_names = c.fetchall()
-    all_column_names = []
+    ####################################################################################################
 
-    for column in tuple_all_column_names:
-        all_column_names.append(column[0])
+    def create_table(self,
+                     table: str) -> None:
+        """Creates a table, if there isn't one already"""
 
-    if not column_name in all_column_names:
-        c.execute(f"INSERT INTO {table} VALUES ('{column_name}', 0)")
+        conn = sqlite3.connect(self.database_path)
+        c = conn.cursor()
+
+        c.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{table}'")
+
+        if not c.fetchone():
+            c.execute(f"""CREATE TABLE {table} (name text,
+                                                amount integer)""")
+
+            conn.commit()
+
+        conn.close()
+
+    ####################################################################################################
+
+    def uses_update(self,
+                    table: str,
+                    column_name: str) -> None:
+        """This function adds up how often a command has been used, but adding +1 on every execution"""
+
+        self.create_table(table)
+
+        conn = sqlite3.connect(self.database_path)
+        c = conn.cursor()
+
+        c.execute(f"SELECT amount FROM {table} WHERE name = '{column_name}'")
+
+        if not c.fetchone():
+            c.execute(f"INSERT INTO {table} VALUES ('{column_name}', 0)")
+
+            conn.commit()
+
+        c.execute(f"SELECT amount FROM {table} WHERE name = '{column_name}'")
+        new_amount = c.fetchone()[0] + 1
+
+        c.execute(f"UPDATE {table} SET amount = '{new_amount}' WHERE name = '{column_name}'")
+
         conn.commit()
 
-    c.execute(f"SELECT amount FROM {table} WHERE name = '{column_name}'")
-    new_amount = c.fetchone()[0]
-
-    new_amount += 1
-
-    c.execute(f"UPDATE {table} SET amount = '{new_amount}' WHERE name = '{column_name}'")
-
-    conn.commit()
-
-    conn.close()
+        conn.close()

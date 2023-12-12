bfa09a1f307e98d5fae3315942ed4f106c0a2094
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2023-12-12 06:07:59 +0100

    covered edge case for emotes

--- cogs/keywords/KeywordSend.py
+++ cogs/keywords/KeywordSend.py
@@ -47,7 +47,7 @@ class KeywordSend(nextcord_C.Cog):
             user_keywords_in_content = []
 
             for keyword in user_keywords:
-                if f" {keyword} " in re.sub('<[^ ]+?>', "", message.content.lower()): # regex -> message without emotes
+                if f" {keyword} " in re.sub('<[^ ]+?>', "", message.content.lower()) or re.sub('<[^ ]+?>', "", message.content.lower()).startswith(f"{keyword} ") or re.sub('<[^ ]+?>', "", message.content.lower()).endswith(f" {keyword}") or re.sub('<[^ ]+?>', "", message.content.lower()) == f"{keyword}": # regex -> message without emotes
                     user_keywords_in_content.append(keyword)
 
             if user_keywords_in_content == []:

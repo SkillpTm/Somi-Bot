0e71d6be3814b5c2a146e2c401acffd8de9df364
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2023-12-13 03:52:17 +0100

    exported webscraping

--- cogs/lastfm/LastFmArtist.py
+++ cogs/lastfm/LastFmArtist.py
@@ -11,7 +11,7 @@ import urllib.parse
 ####################################################################################################
 
 from lib.db_modules import LastFmDB
-from lib.modules import Checks, EmbedFunctions, Get
+from lib.modules import Checks, EmbedFunctions, Webscrape
 from lib.utilities import LASTFM_TIMEFRAMES_ARTIST, LASTFM_TIMEFRAMES_ARTIST_TEXT, LASTFM_COOKIES, LASTFM_HEADERS, SomiBot
 
 
@@ -75,16 +75,16 @@ class LastFmArtist(nextcord_C.Cog):
             await interaction.followup.send(embed=EmbedFunctions().error(f"{member.mention} hasn't listened to the artist `{artist}` in the timeframe: `{LASTFM_TIMEFRAMES_ARTIST_TEXT[timeframe]}`"))
             return
 
-        artist_name, artist_image_url, artist_metadata, album_output, track_output = self.webscrape_artist_page(soup, artist_for_url)
+        type_name, artist_name, cover_image_url, metadata_list, track_output, album_output = Webscrape().library_subpage(soup, artist_for_url, "artist")
 
         embed = EmbedFunctions().builder(
             color = self.client.LASTFM_COLOR,
-            thumbnail = artist_image_url,
-            author = f"{member.display_name} X {artist_name.text}: {LASTFM_TIMEFRAMES_ARTIST_TEXT[timeframe]}",
+            thumbnail = cover_image_url,
+            author = f"{member.display_name} X {type_name.text}: {LASTFM_TIMEFRAMES_ARTIST_TEXT[timeframe]}",
             author_url = f"https://www.last.fm/user/{lastfm_username}/library/music/{artist_for_url}?date_preset={timeframe}",
             author_icon = self.client.LASTFM_ICON,
-            description = f"Total plays: __**{artist_metadata[0]}**__\n" +
-                          f"Listened to: **{artist_metadata[1]}** Albums // **{artist_metadata[2]}** Tracks\n\n" +
+            description = f"Total plays: __**{metadata_list[0]}**__\n" +
+                          f"Listened to: **{metadata_list[1]}** Albums // **{metadata_list[2]}** Tracks\n\n" +
                           f"**Top Albums**\n" +
                           f"{album_output}\n" +
                           f"**Top Tracks**\n" +
@@ -93,62 +93,6 @@ class LastFmArtist(nextcord_C.Cog):
         )
 
         await interaction.followup.send(embed=embed)
-
-    ####################################################################################################
-
-    @staticmethod
-    def webscrape_artist_page(soup: BeautifulSoup, artist_for_url: str) -> tuple[str, str, list[str], str, str]:
-        """This function webscrapes the artist page of a given user"""
-
-        artist_name: str = str(soup.find("h2", class_="library-header-title").text)
-
-        found_image = soup.find("span", class_="avatar library-header-image")
-        artist_image_url: str = str(found_image.img["src"])
-
-        found_metadata = soup.find_all("p", class_="metadata-display")
-        artist_metadata = [str(metadata.text) for metadata in found_metadata] # [Scrobbles, Albums, Tracks]
-
-        found_positions = soup.find_all("td", class_="chartlist-index")
-        positions_list = [str(position.text).replace(" ", "").replace("\n", "") for position in found_positions]
-
-        found_names = soup.find_all("td", class_="chartlist-name")
-        names_list = [str(name.text) for name in found_names if str(name.text).replace("\n", "")]
-
-        found_scorbbles = soup.find_all("span", class_="chartlist-count-bar-value")
-        scorbbles_list = [re.sub(r"[^\d]", "", scrobbles.text) for scrobbles in found_scorbbles]
-
-        album_output = ""
-        track_output = ""
-        current_element_album = 0
-
-        for position, name, scrobbles in zip(positions_list, names_list, scorbbles_list):
-            if int(position) == 1:
-                current_element_album += 1 # setting a flag for if we're working on albums or tracks
-
-            name = re.sub('\n', '', name) # can be both album and track
-
-            safe_name = Get().markdown_safe(name)
-
-            name_for_url = urllib.parse.quote_plus(name)
-
-
-            if int(scrobbles) == 1:
-                scrobbles += " play"
-            else:
-                scrobbles += " plays"
-
-
-            if current_element_album == 1:
-                album_output += f"{position}. "
-                album_output += f"[{safe_name}](https://www.last.fm/music/{artist_for_url}/{name_for_url}/ "
-                album_output += f"- *({scrobbles})*\n"
-            else:
-                track_output += f"{position}. "
-                track_output += f"[{safe_name}](https://www.last.fm/music/{artist_for_url}/_/{name_for_url}/ "
-                track_output += f"- *({scrobbles})*\n"
-        
-        return artist_name, artist_image_url, artist_metadata, album_output, track_output
-
 
 
 def setup(client: SomiBot):

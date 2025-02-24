fd67385c9feb186ec3555755fd1572ccd81a1df1
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2025-02-24 04:07:50 +0100

    added guild_ids to config and chnaged formating

--- lib/utilities/main_commands.py
+++ lib/utilities/main_commands.py
@@ -10,38 +10,51 @@ client = SomiBot()
 
 ####################################################################################################
 
+@client.slash_command(
+    name='manage',
+    guild_ids = client.guilds,
+    default_member_permissions=nextcord.Permissions(manage_guild=True),
+    name_localizations = {country_tag:"config" for country_tag in nextcord.Locale}
+)
+async def config(self, interaction: nextcord.Interaction) -> None:
+    pass
 
-@client.slash_command(name='custom', default_member_permissions=nextcord.Permissions(manage_messages=True, manage_guild=True))
-async def custom(self, interaction: nextcord.Interaction):
+####################################################################################################
+
+@client.slash_command(
+    name='custom',
+    default_member_permissions=nextcord.Permissions(manage_messages=True, manage_guild=True)
+)
+async def custom(self, interaction: nextcord.Interaction) -> None:
     pass
 
 ####################################################################################################
 
 
-@client.slash_command(name='noti', name_localizations = {country_tag:"keyword" for country_tag in nextcord.Locale})
-async def keyword(self, interaction: nextcord.Interaction):
+@client.slash_command(
+    name='noti',
+    name_localizations = {country_tag:"keyword" for country_tag in nextcord.Locale}
+)
+async def keyword(self, interaction: nextcord.Interaction) -> None:
     pass
 
 ####################################################################################################
 
 @client.slash_command(name='levels')
-async def levels(self, interaction: nextcord.Interaction):
+async def levels(self, interaction: nextcord.Interaction) -> None:
     pass
 
 ####################################################################################################
 
 @client.slash_command(name='lf')
-async def lastfm(self, interaction: nextcord.Interaction):
+async def lastfm(self, interaction: nextcord.Interaction) -> None:
     pass
 
 ####################################################################################################
 
-@client.slash_command(name='alarm', name_localizations = {country_tag:"reminder" for country_tag in nextcord.Locale})
-async def reminder(self, interaction: nextcord.Interaction):
-    pass
-
-####################################################################################################
-
-@client.slash_command(name='manage', default_member_permissions=nextcord.Permissions(manage_guild=True), name_localizations = {country_tag:"config" for country_tag in nextcord.Locale})
-async def config(self, interaction: nextcord.Interaction):
+@client.slash_command(
+    name='alarm',
+    name_localizations = {country_tag:"reminder" for country_tag in nextcord.Locale}
+)
+async def reminder(self, interaction: nextcord.Interaction) -> None:
     pass

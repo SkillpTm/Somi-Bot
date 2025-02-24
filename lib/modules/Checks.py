9718e1a22b6a80aca24339cddfe88ed1e3fa7e5d
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2025-02-24 02:19:30 +0100

    seperated is bot and in guild

--- lib/modules/Checks.py
+++ lib/modules/Checks.py
@@ -10,6 +10,7 @@ from lib.utilities.SomiBot import SomiBot
 
 
 class Checks():
+    """This class supplies several static check methodes"""
 
     def __init__(self) -> None:
         pass
@@ -18,19 +19,42 @@ class Checks():
 
     @staticmethod
     def interaction_in_guild() -> bool:
-        """This function checks, if an interaction occured in a guild and wasn't initiate by a bot"""
+        """This function checks, if an interaction occured in a guild"""
 
         def predicate(interaction: nextcord.Interaction) -> bool:
-            return interaction.guild in interaction.client.guilds and interaction.user.bot == False
+            return interaction.guild in interaction.client.guilds
+        
         return nextcord_AC.check(predicate)
 
     ####################################################################################################
 
     @staticmethod
-    def message_in_guild(client: SomiBot,
-                         message: nextcord.Message) -> bool:
-        """This function checks, if a message was send in a guild and wasn't send by a bot"""
+    def message_in_guild(
+        client: SomiBot,
+        message: nextcord.Message
+    ) -> bool:
+        """This function checks, if a message was send in a guild"""
 
-        return message.guild in client.guilds and message.author.bot == False
+        return message.guild in client.guilds
 
+    ####################################################################################################
+
+
+    @staticmethod
+    def interaction_by_bot() -> bool:
+        """This function checks, if an interaction was created by a bot"""
+
+        def predicate(interaction: nextcord.Interaction) -> bool:
+            return interaction.user.bot
+        
+        return predicate
     
+    ####################################################################################################
+
+    @staticmethod
+    def message_by_bot(
+        message: nextcord.Message
+    ) -> bool:
+        """This function checks, if a message was send by a bot"""
+
+        return message.author.bot == False

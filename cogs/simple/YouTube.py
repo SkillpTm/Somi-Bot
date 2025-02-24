32d6603ff5c49ca3e07398e6db306501c1d2e423
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2025-02-24 22:46:00 +0100

    changed formatting and put inital message send into main function

--- cogs/simple/YouTube.py
+++ cogs/simple/YouTube.py
@@ -6,27 +6,38 @@ import nextcord.ext.application_checks as nextcord_AC
 
 ####################################################################################################
 
-from lib.modules import Checks, EmbedFunctions
+from lib.modules import Checks, EmbedFunctions, Get
 from lib.utilities import PageButtons, SomiBot
 
 
 
 class YouTube(nextcord_C.Cog):
 
-    def __init__(self, client):
+    def __init__(self, client) -> None:
         self.client: SomiBot = client
 
     ####################################################################################################
 
     @nextcord.slash_command(name = "yt", description = "find a video on YouTube", name_localizations = {country_tag:"youtube" for country_tag in nextcord.Locale})
-    @nextcord_AC.check(Checks().interaction_in_guild())
-    async def youtube(self,
-                     interaction: nextcord.Interaction,
-                     *,
-                     query: str = nextcord.SlashOption(description="your YouTube Search", required=True, min_length=2, max_length=200)):
+    @nextcord_AC.check(Checks().interaction_not_by_bot())
+    async def youtube(
+        self,
+        interaction: nextcord.Interaction,
+        *,
+        query: str = nextcord.SlashOption(
+            description="your YouTube Search",
+            required=True,
+            min_length=2,
+            max_length=200
+        )
+    ) -> None:
         """This command uses the given query and sends it to the YouTube Data API, then the user can flip thourgh the top 50 results"""
 
-        self.client.Loggers.action_log(f"Guild: {interaction.guild.id} ~ Channel: {interaction.channel.id} ~ User: {interaction.user.id} ~ /youtube {query}")
+        self.client.Loggers.action_log(Get().interaction_log_message(
+            interaction,
+            "/youtube",
+            {"query": query}
+        ))
 
         await interaction.response.defer(with_message = True)
 
@@ -37,25 +48,24 @@ class YouTube(nextcord_C.Cog):
         if results == []:
             await interaction.followup.send(embed=EmbedFunctions().error(f"YouTube couldn't find a video for your query:\n`{query}`"))
             return
+        
+        await interaction.followup.send(content=results[0])
 
-        await self.youtube_rec(interaction=interaction, results=results, page_number=1, first_message_sent=False)
+        await self.youtube_rec(interaction=interaction, results=results, page_number=1)
     
     ####################################################################################################
 
-    async def youtube_rec(self,
-                          interaction: nextcord.Interaction,
-                          results: list[str],
-                          page_number: int,
-                          first_message_sent: bool):
+    async def youtube_rec(
+        self,
+        interaction: nextcord.Interaction,
+        results: list[str],
+        page_number: int
+    ) -> None:
         """This function recurses on button press and flips through the results gained earlier"""
 
         view = PageButtons(page = page_number, last_page = len(results), interaction = interaction)
 
-        if not first_message_sent:
-            first_message_sent = True
-            await interaction.followup.send(content=results[page_number-1], view=view)
-        else:
-            await interaction.edit_original_message(content=results[page_number-1], view=view)
+        await interaction.edit_original_message(content=results[page_number-1], view=view)
 
         await view.update_buttons()
         await view.wait()
@@ -63,9 +73,9 @@ class YouTube(nextcord_C.Cog):
         if not view.value:
             return
 
-        await self.youtube_rec(interaction=interaction, results=results, page_number=view.page, first_message_sent=first_message_sent)
+        await self.youtube_rec(interaction=interaction, results=results, page_number=view.page)
 
 
 
-def setup(client: SomiBot):
+def setup(client: SomiBot) -> None:
     client.add_cog(YouTube(client))

de78c8740192bf199a3f175e0db1b71c70e20169
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2025-02-24 04:27:12 +0100

    changed formating and exported functions

--- cogs/config/ConfigAuditLogChannel.py
+++ cogs/config/ConfigAuditLogChannel.py
@@ -7,14 +7,14 @@ import nextcord.ext.application_checks as nextcord_AC
 ####################################################################################################
 
 from lib.db_modules import ConfigDB
-from lib.modules import Checks, EmbedFunctions
+from lib.modules import Checks, EmbedFunctions, Get
 from lib.utilities import TEXT_CHANNELS, SomiBot
 
 
 
 class ConfigAuditLogChannel(nextcord_C.Cog):
 
-    def __init__(self, client):
+    def __init__(self, client) -> None:
         self.client: SomiBot = client
 
     from lib.utilities.main_commands import config
@@ -22,46 +22,34 @@ class ConfigAuditLogChannel(nextcord_C.Cog):
     ####################################################################################################
 
     @config.subcommand(name = "audit-log-channel", description = "set/unset a channel for the bot to post logs")
-    @nextcord_AC.check(Checks().interaction_in_guild())
-    async def config_audit_log_channel(self,
-                                       interaction: nextcord.Interaction,
-                                       *,
-                                       action: str = nextcord.SlashOption(description="which action do you want to take", required=True, choices=["Set", "Unset"]),
-                                       channel: nextcord.abc.GuildChannel = nextcord.SlashOption(channel_types=TEXT_CHANNELS, description="the channel to be set/unset", required=False)):
+    @nextcord_AC.check(Checks().interaction_not_by_bot() and Checks().interaction_in_guild)
+    async def config_audit_log_channel(
+        self,
+        interaction: nextcord.Interaction,
+        *,
+        action: str = nextcord.SlashOption(description="which action do you want to take", required=True, choices=["Set", "Unset"]),
+        channel: nextcord.abc.GuildChannel = nextcord.SlashOption(channel_types=TEXT_CHANNELS, description="the channel to be set/unset", required=False)
+    ) -> None:
         """This command sets/unsets the audit-log-channel of the server."""
 
         if not channel:
             channel = interaction.channel
 
-        self.client.Loggers.action_log(f"Guild: {interaction.guild.id} ~ Channel: {interaction.channel.id} ~ User: {interaction.user.id} ~ /config audit-log-channel {action}: {channel.id}")
+        self.client.Loggers.action_log(Get().interaction_log_message(interaction, "/config audit-log-channel", {action: str(channel.id)}))
 
         await interaction.response.defer(ephemeral=True, with_message=True)
 
 
         if action == "Set":
-            added = ConfigDB(interaction.guild.id, "AuditLogChannel").add(channel.id)
-
-            if not added:
-                await interaction.followup.send(embed=EmbedFunctions().error(f"This server already has an audit-log-channel.\nUnset your audit-log-channel first."), ephemeral=True)
-                return
-                
-            await interaction.followup.send(embed=EmbedFunctions().success(f"{channel.mention} is from now on this server's audit-log-channel."), ephemeral=True)
-
-            audit_log_channel = channel
+            await self.setChannel(interaction, channel)
             mod_action = f"{interaction.user.mention} set: {channel.mention} as the new audit-log-channel."
 
 
         elif action == "Unset":
+            # we get the channel_id from the db in case the current audit log channel was deleted and "channel" is unreliable
             channel_id: int = await ConfigDB(interaction.guild.id, "AuditLogChannel").get_list(interaction.guild)
-            deleted = ConfigDB(interaction.guild.id, "AuditLogChannel").delete(channel.id)
+            await self.unsetChannel(interaction, channel_id)
 
-            if not deleted:
-                await interaction.followup.send(embed=EmbedFunctions().error("This server doesn't have an audit-log-channel."), ephemeral=True)
-                return
-            
-            await interaction.followup.send(embed=EmbedFunctions().success("You successfully unset this server's audit-log-channel."), ephemeral=True)
-
-            audit_log_channel = interaction.guild.get_channel(channel_id)
             mod_action = f"{interaction.user.mention} unset: <#{channel_id}> as the audit-log-channel."
 
 
@@ -79,9 +67,46 @@ class ConfigAuditLogChannel(nextcord_C.Cog):
             ]
         )
 
-        await audit_log_channel.send(embed=embed)
+        await channel.send(embed=embed)
+
+    ####################################################################################################
+
+    @staticmethod
+    async def setChannel(
+        interaction: nextcord.Interaction,
+        channel: nextcord.abc.GuildChannel
+    ) -> bool:
+        "sets or doesn't set the channel indicated by the output bool"
+
+        added = ConfigDB(interaction.guild.id, "AuditLogChannel").add(channel.id)
+
+        if not added:
+            await interaction.followup.send(embed=EmbedFunctions().error(f"This server already has an audit-log-channel.\nUnset your audit-log-channel first."), ephemeral=True)
+            return added
+            
+        await interaction.followup.send(embed=EmbedFunctions().success(f"{channel.mention} is from now on this server's audit-log-channel."), ephemeral=True)
+        return added
+    
+    ####################################################################################################
+
+    @staticmethod
+    async def unsetChannel(
+        interaction: nextcord.Interaction,
+        channel_id: int
+    ) -> bool:
+        "unsets or doesn't unset the channel indicated by the output bool"
+        
+        deleted = ConfigDB(interaction.guild.id, "AuditLogChannel").delete(channel_id)
+
+        if not deleted:
+            await interaction.followup.send(embed=EmbedFunctions().error("This server doesn't have an audit-log-channel."), ephemeral=True)
+            return deleted
+        
+        await interaction.followup.send(embed=EmbedFunctions().success("You successfully unset this server's audit-log-channel."), ephemeral=True)
+        return deleted
 
 
 
-def setup(client: SomiBot):
+
+def setup(client: SomiBot) -> None:
     client.add_cog(ConfigAuditLogChannel(client))

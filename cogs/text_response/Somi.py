a6c2a476b7a7303232bb6868ccce73da364454f3
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2025-02-24 20:46:55 +0100

    changed formatting

--- cogs/text_response/Somi.py
+++ cogs/text_response/Somi.py
@@ -6,29 +6,31 @@ import nextcord.ext.application_checks as nextcord_AC
 
 ####################################################################################################
 
-from lib.modules import Checks
+from lib.modules import Checks, Get
 from lib.utilities import SomiBot
 
 
 
 class Somi(nextcord_C.Cog):
 
-    def __init__(self, client):
+    def __init__(self, client) -> None:
         self.client: SomiBot = client
 
     ####################################################################################################
 
     @nextcord.slash_command(name = "somi", description = "speaks facts")
-    @nextcord_AC.check(Checks().interaction_in_guild())
-    async def somi(self,
-                   interaction: nextcord.Interaction):
+    @nextcord_AC.check(Checks().interaction_not_by_bot())
+    async def somi(
+        self,
+        interaction: nextcord.Interaction
+    ) -> None:
         """This command tells you the truth"""
 
-        self.client.Loggers.action_log(f"Guild: {interaction.guild.id} ~ Channel: {interaction.channel.id} ~ User: {interaction.user.id} ~ /somi")
+        self.client.Loggers.action_log(Get().interaction_log_message(interaction, "/somi"))
 
         await interaction.response.send_message(f"Somi best grill\n{self.client.SOMI_BEST_GRILL_IMAGE}")
 
 
 
-def setup(client: SomiBot):
+def setup(client: SomiBot) -> None:
     client.add_cog(Somi(client))

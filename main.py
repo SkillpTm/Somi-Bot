39f01e5bc7264e76d7d2b33bce18dcc699b4d788
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2025-02-24 03:49:19 +0100

    added name main and put functionality into functions

--- main.py
+++ main.py
@@ -13,21 +13,35 @@ client = SomiBot()
 
 ####################################################################################################
 
-for folder in os.listdir(f"./cogs/"):
-    if not os.path.isdir(f"./cogs/{folder}/"):
-        continue
-    
-    for file in os.listdir(f"./cogs/{folder}/"):
-        if not file.endswith(".py"):
-            continue
+def load_Cogs() -> None:
+    """loads all cogs onto the client"""
 
-        client.load_extension(f"cogs.{folder}.{file[:-3]}")
+    # crawl through ./cogs/ 's subfolders to load all cogs
+    for folder in os.listdir(f"./cogs/"):
+        if not os.path.isdir(f"./cogs/{folder}/"):
+            continue
+        
+        for file in os.listdir(f"./cogs/{folder}/"):
+            if not file.endswith(".py"):
+                continue
+
+            client.load_extension(f"cogs.{folder}.{file[:-3]}")
 
 ####################################################################################################
 
-try:
-    requests.get("https://www.google.com/")
-    client.run(client.Keychain.DISCORD_TOKEN)
-except (requests.ConnectionError, aiohttp.client_exceptions.ClientConnectorError):
-    time.sleep(10)
-    client.restart()
+def start() -> None:
+    """starts the bot indefinitly"""
+
+    # if there is no internet connection indefinitly restart the bot
+    try:
+        requests.get("https://www.google.com/")
+        client.run(client.Keychain.DISCORD_TOKEN)
+    except (requests.ConnectionError, aiohttp.client_exceptions.ClientConnectorError):
+        time.sleep(10)
+        client.restart()
+
+####################################################################################################
+
+if __name__ == "__main__":
+    load_Cogs()
+    start() # blocks until the bot is shutdown

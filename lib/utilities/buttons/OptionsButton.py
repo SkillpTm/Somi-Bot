ca279f85aa23bb3222452ed7b0682c7990a6eda2
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2025-02-25 03:20:47 +0100

    changed formatting

--- lib/utilities/buttons/OptionsButton.py
+++ lib/utilities/buttons/OptionsButton.py
@@ -10,8 +10,7 @@ from lib.utilities.SomiBot import SomiBot
 
 class OptionsButton(nextcord.ui.View):
     
-    def __init__(self,
-                 interaction: nextcord.Interaction = None):
+    def __init__(self, interaction: nextcord.Interaction = None) -> None:
         super().__init__(timeout = 60)
         self.interaction = interaction
         self.value: bool = None
@@ -19,14 +18,16 @@ class OptionsButton(nextcord.ui.View):
     ####################################################################################################
 
     @nextcord.ui.button(label = "All Options", style=nextcord.ButtonStyle.gray)
-    async def all_options(self,
-                  button: nextcord.ui.Button,
-                  interaction: nextcord.Interaction):
+    async def all_options(
+        self,
+        button: nextcord.ui.Button,
+        interaction: nextcord.Interaction
+    ) -> None:
         self.value = True
         self.stop()
         await SomiBot.deactivate_view_children(self)
 
     ####################################################################################################
 
-    async def on_timeout(self):
+    async def on_timeout(self) -> None:
         await SomiBot.deactivate_view_children(self)

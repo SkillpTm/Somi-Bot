cc5cb13245e5a2dd756f38a6e677bd7fb70a8f0c
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2025-02-27 07:06:09 +0100

    changed formatting and removed on_message to consolidate on the client overwrite in SomiBot.py soon

--- cogs/level/LevelsGainXp.py
+++ cogs/level/LevelsGainXp.py
@@ -14,14 +14,12 @@ from lib.utilities import SomiBot
 
 class LevelsGainXp(nextcord_C.Cog):
 
-    def __init__(self, client):
+    def __init__(self, client) -> None:
         self.client: SomiBot = client
 
     ####################################################################################################
 
-    @nextcord_C.Cog.listener()
-    async def on_message(self,
-                         message: nextcord.Message):
+    async def levels_gain_xp(self, message: nextcord.Message) -> None:
         """This command gives xp to a user on the server the message was send, if the message:
             1. is longer than 10 characters
             2. was not send in a levels ignore channel
@@ -32,22 +30,22 @@ class LevelsGainXp(nextcord_C.Cog):
         if not Checks.message_in_guild(self.client, message):
             return
 
+        # re removes emotes, make sure messages are above 10 chars
         if not len(re.sub('<[^ ]+?>', "",message.content)) > 10:
             return
-        
-        ignore_channel_ids = await ConfigDB(message.guild.id, "LevelIgnoreChannels").get_list(message.guild)
 
-        if message.channel.id in ignore_channel_ids:
+        # make sure message isn't in a levels ignore channel
+        if message.channel.id in await ConfigDB(message.guild.id, "LevelIgnoreChannels").get_list(message.guild):
             return
 
         if not LevelsDB(message.guild.id).increase_xp(message.author.id):
             return
 
-        user_level, xp_until_next_level =  LevelsDB(message.guild.id).get_level(message.author.id)
+        user_level, _ =  LevelsDB(message.guild.id).get_level(message.author.id)
 
         await LevelRoles().apply(message.guild, [[message.author.id, user_level]])
 
 
 
-def setup(client: SomiBot):
+def setup(client: SomiBot) -> None:
     client.add_cog(LevelsGainXp(client))

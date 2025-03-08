ddfd020ff7d2a15d7694d0ef619b92b5be9aee2b
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2025-03-08 05:37:45 +0100

    added .env.example and moved LF cookies/headers into the env, with that also removed env.json

--- lib/utilities/Keychain.py
+++ lib/utilities/Keychain.py
@@ -10,23 +10,17 @@ class Keychain():
     dotenv.load_dotenv()
 
     DISCORD_TOKEN: str = os.getenv("DISCORD_TOKEN")
-    
-    LAST_FM_API_KEY: str = os.getenv("LAST_FM_API_KEY")
-
-    with open('env.json') as env_json:
-        env_json_data = json.loads(env_json.read())
-    
-    LAST_FM_COOKIES = env_json_data["LAST_FM_COOKIES"]
-    LAST_FM_HEADERS = env_json_data["LAST_FM_HEADERS"]
 
     SPOTIPY_CLIENT_ID: str = os.getenv("SPOTIPY_CLIENT_ID")
     SPOTIPY_CLIENT_SECRET: str = os.getenv("SPOTIPY_CLIENT_SECRET")
     SPOTIPY_REDIRECT_URI: str = os.getenv("SPOTIPY_REDIRECT_URI")
 
     GENIUS_ACCESS_TOKEN: str = os.getenv("GENIUS_ACCESS_TOKEN")
-
+    LAST_FM_API_KEY: str = os.getenv("LAST_FM_API_KEY")
     WEATHER_API_KEY: str = os.getenv("WEATHER_API_KEY")
-
     WOLFRAM_APP_ID: str = os.getenv("WOLFRAM_APP_ID")
-
     YOUTUBE_API_KEY: str = os.getenv("YOUTUBE_API_KEY")
+
+    # Webscraping
+    LAST_FM_COOKIES = json.loads(os.getenv("LAST_FM_COOKIES"))
+    LAST_FM_HEADERS = json.loads(os.getenv("LAST_FM_HEADERS"))

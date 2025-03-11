38a3f6a164f653a0392f08e3ee849e2b100f5c5c
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2025-03-11 11:39:19 +0100

    made sub functions for set and get

--- lib/dbModules/tableInterfaces/ServerDB.py
+++ lib/dbModules/tableInterfaces/ServerDB.py
@@ -11,76 +11,77 @@ class ServerDB():
 
     ####################################################################################################
 
-    async def audit_log_set(self, audit_log_channel_id: int) -> None:
-        """adds the audit log to the server in the db"""
+    async def _get(self, select_columns: list[str]) -> int:
+        """get selected column value from the server"""
+
+        return await self.database.fetch_val(
+            query_name = "select_where",
+            table_name = "server",
+            select_columns = select_columns,
+            columns = ["server_id"],
+            values = [self.server_id]
+        )
+
+    ####################################################################################################
+
+    async def _set(self, set_columns: list[str], set_values: list[int]) -> str:
+        """set the column values for the server"""
 
         await self.database.execute(
             query_name = "update_rows_where",
             table_name = "server",
             columns = ["server_id"],
             values = [self.server_id],
-            set_columns = ["audit_log_channel_id"],
-            set_values = [audit_log_channel_id]
+            set_columns = set_columns,
+            set_values = set_values
         )
+
+    ####################################################################################################
+
+    async def audit_log_get(self) -> int:
+        """get the audit log id from the db"""
+
+        return await self._get(["audit_log_channel_id"])
+
+    ####################################################################################################
+
+    async def audit_log_set(self, audit_log_channel_id: int) -> None:
+        """adds the audit log id to the server in the db"""
+
+        await self._set(["audit_log_channel_id"], [audit_log_channel_id])
 
     ####################################################################################################
 
     async def audit_log_reset(self) -> bool:
-        """deletes the audit log from the server in the db"""
+        """deletes the audit log id from the server in the db"""
 
         if not await self.audit_log_get():
             return False
 
-        await self.audit_log_set(None)
+        await self.audit_log_set(0)
         return True
-    
+
     ####################################################################################################
 
-    async def audit_log_get(self) -> int:
-        """get the audit log from the db"""
+    async def default_role_get(self) -> int:
+        """get the default role id from the db"""
 
-        return await self.database.fetch_val(
-            query_name = "select_where",
-            table_name = "server",
-            select_columns=["audit_log_channel_id"],
-            columns = ["server_id"],
-            values = [self.server_id]
-        )
-    
+        return await self._get(["default_role_id"])
+
     ####################################################################################################
 
     async def default_role_set(self, default_role_id: int) -> None:
-        """adds the default role to the server in the db"""
+        """adds the default role id to the server in the db"""
 
-        await self.database.execute(
-            query_name = "update_rows_where",
-            table_name = "server",
-            columns = ["server_id"],
-            values = [self.server_id],
-            set_columns = ["default_role_id"],
-            set_values = [default_role_id]
-        )
+        await self._set(["default_role_id"], [default_role_id])
 
     ####################################################################################################
 
     async def default_role_reset(self) -> bool:
-        """deletes the default role from the server in the db"""
+        """deletes the default role id from the server in the db"""
 
         if not await self.default_role_get():
             return False
 
-        await self.default_role_set(None)
+        await self.default_role_set(0)
         return True
-    
-    ####################################################################################################
-
-    async def default_role_get(self) -> int:
-        """get the default role from the db"""
-
-        return await self.database.fetch_val(
-            query_name = "select_where",
-            table_name = "server",
-            select_columns=["default_role_id"],
-            columns = ["server_id"],
-            values = [self.server_id]
-        )
--- lib/dbModules/tableInterfaces/UserDB.py
+++ lib/dbModules/tableInterfaces/UserDB.py
@@ -11,61 +11,66 @@ class UserDB():
 
     ####################################################################################################
 
-    async def last_fm_set(self, last_fm_username: str) -> None:
-        """adds the lastfm username to the user in the db"""
+    async def _get(self, select_columns: list[str]) -> str:
+        """get selected column value from the user"""
+
+        return await self.database.fetch_val(
+            query_name = "select_where",
+            table_name = "user",
+            select_columns = select_columns,
+            columns = ["user_id"],
+            values = [self.user_id]
+        )
+
+    ####################################################################################################
+
+    async def _set(self, set_columns: list[str], set_values: list[str]) -> None:
+        """set the column values for the user"""
 
         await self.database.execute(
             query_name = "update_rows_where",
             table_name = "user",
             columns = ["user_id"],
             values = [self.user_id],
-            set_columns = ["last_fm_username"],
-            set_values = [last_fm_username]
+            set_columns = set_columns,
+            set_values = set_values
         )
 
-    ####################################################################################################
-
-    async def last_fm_reset(self) -> None:
-        """deletes the lastfm username from the user in the db"""
-
-        return await self.last_fm_set("")
-    
     ####################################################################################################
 
     async def last_fm_get(self) -> str:
         """get the lastfm username from the db"""
 
-        return await self.database.fetch_val(
-            query_name = "select_where",
-            table_name = "user",
-            select_columns=["last_fm_username"],
-            columns = ["user_id"],
-            values = [self.user_id]
-        )
-    
+        return await self._get(["last_fm_username"])
+
     ####################################################################################################
 
-    async def weather_set(self, weather_location: str) -> None:
-        """adds the weasther location to the user in the db"""
+    async def last_fm_set(self, last_fm_username: str) -> None:
+        """adds the lastfm username to the user in the db"""
 
-        await self.database.execute(
-            query_name = "update_rows_where",
-            table_name = "user",
-            columns = ["user_id"],
-            values = [self.user_id],
-            set_columns = ["weather_location"],
-            set_values = [weather_location]
-        )
+        await self._set(["last_fm_username"], [last_fm_username])
+
+    ####################################################################################################
+
+    async def last_fm_reset(self) -> bool:
+        """deletes the lastfm username from the user in the db"""
+
+        if not await self.last_fm_get():
+            return False
+
+        await self.last_fm_set("")
+        return True
 
     ####################################################################################################
 
     async def weather_get(self) -> str:
         """get the user's weather location from the db"""
 
-        return await self.database.fetch_val(
-            query_name = "select_where",
-            table_name = "user",
-            select_columns=["weather_location"],
-            columns = ["user_id"],
-            values = [self.user_id]
-        )
+        return await self._get(["weather_location"])
+
+    ####################################################################################################
+
+    async def weather_set(self, weather_location: str) -> None:
+        """adds the weather location to the user in the db"""
+
+        self._set(["weather_location"], [weather_location])

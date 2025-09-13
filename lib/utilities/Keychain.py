4469ea247dde39257f379f3dc8747318649cbf22
SkillpTm <99091714+SkillpTm@users.noreply.github.com>
2025-09-13 20:25:25 +0200

    lambda keychain gets so they only get loaded on Keychain init

--- lib/utilities/Keychain.py
+++ lib/utilities/Keychain.py
@@ -6,18 +6,18 @@ import os
 
 @dataclasses.dataclass
 class Keychain():
-    DISCORD_TOKEN: str = os.getenv("DISCORD_TOKEN")
+    DISCORD_TOKEN: str = dataclasses.field(default_factory = lambda: os.getenv("DISCORD_TOKEN"))
 
-    SPOTIPY_CLIENT_ID: str = os.getenv("SPOTIPY_CLIENT_ID")
-    SPOTIPY_CLIENT_SECRET: str = os.getenv("SPOTIPY_CLIENT_SECRET")
-    SPOTIPY_REDIRECT_URI: str = os.getenv("SPOTIPY_REDIRECT_URI")
+    SPOTIPY_CLIENT_ID: str = dataclasses.field(default_factory = lambda: os.getenv("SPOTIPY_CLIENT_ID"))
+    SPOTIPY_CLIENT_SECRET: str = dataclasses.field(default_factory = lambda: os.getenv("SPOTIPY_CLIENT_SECRET"))
+    SPOTIPY_REDIRECT_URI: str = dataclasses.field(default_factory = lambda: os.getenv("SPOTIPY_REDIRECT_URI"))
 
-    GENIUS_ACCESS_TOKEN: str = os.getenv("GENIUS_ACCESS_TOKEN")
-    LAST_FM_API_KEY: str = os.getenv("LAST_FM_API_KEY")
-    WEATHER_API_KEY: str = os.getenv("WEATHER_API_KEY")
-    WOLFRAM_APP_ID: str = os.getenv("WOLFRAM_APP_ID")
-    YOUTUBE_API_KEY: str = os.getenv("YOUTUBE_API_KEY")
+    GENIUS_ACCESS_TOKEN: str = dataclasses.field(default_factory = lambda: os.getenv("GENIUS_ACCESS_TOKEN"))
+    LAST_FM_API_KEY: str = dataclasses.field(default_factory = lambda: os.getenv("LAST_FM_API_KEY"))
+    WEATHER_API_KEY: str = dataclasses.field(default_factory = lambda: os.getenv("WEATHER_API_KEY"))
+    WOLFRAM_APP_ID: str = dataclasses.field(default_factory = lambda: os.getenv("WOLFRAM_APP_ID"))
+    YOUTUBE_API_KEY: str = dataclasses.field(default_factory = lambda: os.getenv("YOUTUBE_API_KEY"))
 
     # Webscraping
-    LAST_FM_COOKIES = json.loads(os.getenv("LAST_FM_COOKIES"))
-    LAST_FM_HEADERS = json.loads(os.getenv("LAST_FM_HEADERS"))
+    LAST_FM_COOKIES: str = dataclasses.field(default_factory = lambda: json.loads(os.getenv("LAST_FM_COOKIES")))
+    LAST_FM_HEADERS: str = dataclasses.field(default_factory = lambda: json.loads(os.getenv("LAST_FM_HEADERS")))
